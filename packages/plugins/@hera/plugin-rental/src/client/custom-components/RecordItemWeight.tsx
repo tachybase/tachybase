a7bd0695ce1c8cf847d866dca67f9dbd39a4f352
sealday <sealday@gmail.com>
2024-03-08 21:05:56 +0800

    perf: optimize count and weight item

--- packages/plugins/@hera/plugin-rental/src/client/custom-components/RecordItemWeight.tsx
+++ packages/plugins/@hera/plugin-rental/src/client/custom-components/RecordItemWeight.tsx
@@ -1,50 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import {
   CUSTOM_COMPONENT_TYPE_FIELD,
   KEY_CUSTOM_COMPONENT_LABEL,
   KEY_CUSTOM_COMPONENT_TYPE,
 } from '@hera/plugin-core/client';
-import { useField, useForm, useFormEffects } from '@formily/react';
-import { onFieldInit, onFieldValueChange } from '@formily/core';
+import { observer, useField, useForm } from '@formily/react';
 import _ from 'lodash';
 import { formatQuantity } from '../../utils/currencyUtils';
-// 按产品表换算逻辑计算重量
-export const RecordItemWeight = (props) => {
-  const [items, setItems] = useState([]);
-  const [result, setResult] = useState('-');
-  // 先做重量
+
+export const RecordItemWeight = observer((props) => {
   const form = useForm();
   const field = useField();
-  const currentItemsPath = field.path.parent().parent().entire + '.*';
-  const calc = () => {
-    const path = field.path.entire as string;
-    const regx = path.match(/\d+/g);
-    if (!regx) return;
-    const itemiIndex = regx[0];
-    const itemData = items[itemiIndex];
-    if (itemData.product && itemData.count) {
-      const value = ((itemData.product.weight || 0) * itemData.count) / 1000;
-      if (value) {
-        setResult(formatQuantity(value, 2) + '吨');
-      }
+  const item = form.getValuesIn(field.path.slice(0, -2).entire);
+  if (item?.product && item?.count) {
+    const value = ((item.product.weight || 0) * item.count) / 1000;
+    if (value) {
+      return <span>{formatQuantity(value, 2) + '吨'}</span>;
     }
-  };
-
-  useFormEffects(() => {
-    onFieldInit(currentItemsPath, () => {
-      setItems(_.cloneDeep(form.values.items));
-    });
-    onFieldValueChange(currentItemsPath, () => {
-      setItems(_.cloneDeep(form.values.items));
-    });
-  });
-  useEffect(() => {
-    if (items) {
-      calc();
-    }
-  }, [items]);
-  return <span>{result}</span>;
-};
+  }
+  return <span> - </span>;
+});
 
 RecordItemWeight.displayName = 'RecordItemWeight';
 RecordItemWeight[KEY_CUSTOM_COMPONENT_TYPE] = CUSTOM_COMPONENT_TYPE_FIELD;
