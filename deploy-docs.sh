f9ec74fc6ee9a884405406fc11676d5b6a6fdd97
sealday <sealday@gmail.com>
2024-04-03 00:41:55 +0800

    chore: remove unused files

--- deploy-docs.sh
+++
@@ -1,9 +0,0 @@
-cd docs/dist/en-US
-echo "docs.nocobase.com" >> CNAME
-echo "" >> .nojekyll
-git init
-git remote add origin git@github.com:nocobase/docs.nocobase.com.git
-git branch -M main
-git add .
-git commit -m "first commit"
-git push -f origin main
