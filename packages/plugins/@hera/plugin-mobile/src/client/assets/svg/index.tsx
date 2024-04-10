45948e2e78000011f30cd4db38ba7bf1e14d8234
sealday <zhanglin@daoyoucloud.com>
2024-04-10 17:08:40 +0800

    fix: mobile icon build error

--- packages/plugins/@hera/plugin-mobile/src/client/assets/svg/index.tsx
+++ packages/plugins/@hera/plugin-mobile/src/client/assets/svg/index.tsx
@@ -5,9 +5,12 @@ import NoticeSvg from './notice.svg';
 import SwiperSvg from './swiper.svg';
 import TabSearchSvg from './tab-search.svg';
 
-export const loadBase64Icon = (base64: string) => () => (
-  <div dangerouslySetInnerHTML={{ __html: atob(base64.replace('data:image/svg+xml;base64,', '')) }} />
-);
+let convert = (encoded: string) => decodeURI(encoded).replace('data:image/svg+xml,', '');
+if (process.env.NODE_ENV !== 'production') {
+  convert = (encoded: string) => atob(encoded.replace('data:image/svg+xml;base64,', ''));
+}
+
+export const loadBase64Icon = (encoded: string) => () => <div dangerouslySetInnerHTML={{ __html: convert(encoded) }} />;
 
 export const NoticeIcon = (props: any) => <Icon component={loadBase64Icon(NoticeSvg)} {...props} />;
 export const SwiperIcon = (props: any) => <Icon component={loadBase64Icon(SwiperSvg)} {...props} />;
