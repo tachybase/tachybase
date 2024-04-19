31e1d17253649e4eb5e74ab458baa13c9d8a7914
hello@lv <2256334253@qq.com>
2024-04-19 19:45:17 +0800

    feat_phase_2_project (#776)
    Co-authored-by: lyx <2027667395@qq.com>
    Co-authored-by: bai.zixv <bai.zixv@foxmail.com>
    Co-authored-by: sealday <zhanglin@daoyoucloud.com>
    Co-authored-by: wjh <wwwjh0710@163.com>
    Co-authored-by: sealday <sealday@gmail.com>
    Reviewed-on: https://git.daoyoucloud.com/daoyoucloud/tachybase/pulls/776
    Co-authored-by: hello@lv <2256334253@qq.com>
    Co-committed-by: hello@lv <2256334253@qq.com>

--- packages/plugins/@hera/plugin-rental/src/server/sqls/pdf_waybills.sql
+++ packages/plugins/@hera/plugin-rental/src/server/sqls/pdf_waybills.sql
@@ -1,114 +1,64 @@
 SELECT
+  r.number record_number,
+  w.arrival_date, -- 到货日期
+  w.off_date, -- 承运日期
+  w.weight_or_amount, -- 吨/趟
+  w.unit_price, -- 单价
+  w.additional_cost, -- 附加金额
+  w.pay_date, -- 付款日期
+  w.comment,
+  p."name" AS payer_name, -- 付款方
+  c."name" AS payer_company, -- 付款方公司
+  a."name" AS payee_account_name, -- 收款账户
+  a."number" AS payee_account_number, -- 收款账户
+  a.bank AS payee_account_bank, -- 收款名称
+  p2.address AS shipper_address, -- 发货方
+  p2."name" AS shipper_name, -- 发货方单位
+  c3.name AS shipper_company, -- 发货方单位
+  c4."name" AS shipper_contact, -- 发货方联系人
+  c4.phone AS shipper_contact_phone, -- 发货方联系人
+  c2."name" AS consignee_contact, -- 收款人
+  p3.name AS consignee_name, -- 收货方单位
+  p3.address AS consignee_address,
+  c5."name" AS consignee_company, -- 收货方单位
+  c6."name" AS consignee_contact, -- 收货人
+  c6.phone AS consignee_contact_phone,
+  c7.name AS carrier, -- 承运商
+  c8.name AS driver, --	驾驶员
+  c8.phone AS driver_phone,
+  c8.id_card AS driver_idcard, --驾驶员
+  (
+    SELECT
+      STRING_AGG(v.number::TEXT, ', ')
+    FROM
+      record_vehicles rv
+      JOIN vehicles v ON rv.vehicle_id = v.id
+    WHERE
+      r.id = rv.record_id
+  ) AS vehicles,
   (
     SELECT
       waybills_explain
     FROM
       basic_configuration
-  ) side_information,
-  w.*,
-  TO_JSONB(
-    JSONB_SET(
-      TO_JSONB(payer),
-      '{company}',
-      (
-        SELECT
-          TO_JSONB(c2)
-        FROM
-          company c2
-        WHERE
-          payer.company_id = c2.id
-      )
-    )
-  ) AS payer, -- 付款方， 查公司company_id
-  TO_JSONB(payee_account) AS payee_account, -- 收款方账号
-  TO_JSONB(shipper_contact) AS shipper_contact, -- 发货方联系人
-  TO_JSONB(consignee_contact) AS consignee_contact, -- 收货方联系人
-  TO_JSONB(carrier) AS carrier, -- 承运商
-  TO_JSONB(driver) AS driver, --司机
-  TO_JSONB(
-    JSONB_SET(
-      TO_JSONB(in_stock),
-      '{company}',
-      (
-        SELECT
-          TO_JSONB(c)
-        FROM
-          company c
-        WHERE
-          in_stock.company_id = c.id
-      )
-    )
-  ) AS in_stock,
-  TO_JSONB(
-    JSONB_SET(
-      TO_JSONB(out_stock),
-      '{company}',
-      (
-        SELECT
-          TO_JSONB(c)
-        FROM
-          company c
-        WHERE
-          out_stock.company_id = c.id
-      )
-    )
-  ) AS out_stock,
-  TO_JSONB(records.*) || JSONB_BUILD_OBJECT(
-    'items',
-    (
-      SELECT
-        JSONB_AGG(
-          TO_JSONB(record_items) || JSONB_BUILD_OBJECT(
-            'product',
-            TO_JSONB(product.*) || JSONB_BUILD_OBJECT('category', TO_JSONB(product_category.*))
-          )
-        )
-      FROM
-        record_items
-        LEFT JOIN product ON record_items.product_id = product.id
-        LEFT JOIN product_category ON product.category_id = product_category.id
-      WHERE
-        records.id = record_items.record_id
-    ),
-    'vehicles',
-    (
-      SELECT
-        COALESCE(JSONB_AGG(v), '[]'::jsonb)
-      FROM
-        record_vehicles rv
-        JOIN vehicles v ON rv.vehicle_id = v.id
-      WHERE
-        records.id = rv.record_id
-    )
-  ) AS record
+  ) side_information
 FROM
   waybills w
-  LEFT JOIN
-  -- 	付款方
-  project payer ON w.payer_id = payer.id
-  LEFT JOIN
-  -- 收款方账户
-  account payee_account ON w.payee_account_id = payee_account.id
-  LEFT JOIN
-  -- 发货方联系人
-  contacts shipper_contact ON w.shipper_contact_id = shipper_contact.id
-  LEFT JOIN
-  -- 收货方联系人
-  contacts consignee_contact ON w.consignee_contact_id = consignee_contact.id
-  LEFT JOIN
-  -- 承运商
-  company carrier ON w.carrier_id = carrier.id
-  LEFT JOIN
-  -- 司机
-  contacts driver ON w.driver_id = driver.id
-  JOIN
-  -- 订单信息
-  records ON w.record_id = records.id
-  JOIN record_items ON record_items.record_id = w.record_id
-  JOIN product ON product.id = record_items.product_id
-  JOIN product_category ON product_category.id = product.category_id
-  LEFT JOIN record_vehicles ON records.id = record_vehicles.record_id
-  LEFT JOIN project in_stock ON records.in_stock_id = in_stock.id
-  LEFT JOIN project out_stock ON records.out_stock_id = out_stock.id
+  JOIN records r ON r.id = w.record_id
+  LEFT JOIN record_contract rc ON r.id = rc.record_id
+  LEFT JOIN project p ON w.payer_id = p.id
+  LEFT JOIN company c ON p.company_id = c.id
+  LEFT JOIN account a ON w.payee_account_id = a.id
+  LEFT JOIN contacts c2 ON w.consignee_contact_id = c2.id
+  JOIN project p2 ON r.out_stock_id = p2.id -- 发货方
+  LEFT JOIN company c3 ON p2.company_id = c3.id
+  LEFT JOIN contacts c4 ON w.shipper_contact_id = c4.id
+  JOIN project p3 ON r.in_stock_id = p3.id -- 收货方
+  LEFT JOIN company c5 ON p3.company_id = c5.id
+  LEFT JOIN contacts c6 ON w.consignee_contact_id = c6.id
+  LEFT JOIN company c7 ON w.carrier_id = c7.id
+  LEFT JOIN contacts c8 ON w.driver_id = c8.id
 WHERE
   w.id = :recordId
+LIMIT
+  1 --rc数量不为1
