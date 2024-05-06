19b92d723e250825de4b54d8868e0afe5bd69c1d
hello@lv <2256334253@qq.com>
2024-05-06 16:51:55 +0800

    feat: 出库（合同）查询 (#911)
    Co-authored-by: lyx <2027667395@qq.com>
    Reviewed-on: https://git.daoyoucloud.com/daoyoucloud/tachybase/pulls/911
    Reviewed-by: sealday <zhanglin@daoyoucloud.com>
    Co-authored-by: hello@lv <2256334253@qq.com>
    Co-committed-by: hello@lv <2256334253@qq.com>

--- packages/plugins/@hera/plugin-rental/src/server/sqls/view_records_contracts.sql
+++ packages/plugins/@hera/plugin-rental/src/server/sqls/view_records_contracts.sql
@@ -2,18 +2,19 @@ CREATE OR REPLACE VIEW
   public.view_records_contracts AS
 SELECT
   c.id AS contract_id,
+  c.record_category AS contract_type,
   r.id,
   r."number",
   r.has_receipt,
   r.has_stub,
   r."date",
   r.original_number,
-  p2."name" AS contract_party,
+  c1."name" AS company_name,
   r."comment",
   r.all_price,
   r.weight,
   rc.movement,
-  p."name" AS contract_project,
+  p.name AS project_name,
   (
     SELECT
       COALESCE(STRING_AGG(v."number", ' '), '')
@@ -28,26 +29,33 @@ FROM
   JOIN record_contract rc ON rc.record_id = r.id
   JOIN contracts c ON c.id = rc.contract_id
   JOIN company c1 ON c1.id = c.first_party_id
-  JOIN project p ON p.company_id = c1.id
-  LEFT JOIN project p2 ON c.project_id = p2.id
+  JOIN project p ON p.id = (
+    CASE
+      WHEN rc.movement = '1' THEN r.out_stock_id
+      ELSE r.in_stock_id
+    END
+  )
 UNION ALL
 SELECT
-  c.id AS contract_id,
+  NULL AS contract_id,
+  NULL AS contract_type,
   r.id,
   r."number",
   r.has_receipt,
   r.has_stub,
   r."date",
   r.original_number,
-  p2."name" AS contract_party,
+  c.name AS company_name,
   r."comment",
   r.all_price,
   r.weight,
-  CASE
-    WHEN rc.movement = '1' THEN '-1'
-    ELSE '1'
-  END,
-  p."name" AS contract_project,
+  (
+    CASE
+      WHEN p3.id = r.in_stock_id THEN '1'
+      ELSE '-1'
+    END
+  ) AS movement,
+  p3."name" AS project_name,
   (
     SELECT
       COALESCE(STRING_AGG(v."number", ' '), '')
@@ -59,8 +67,16 @@ SELECT
   ) AS vehicle_number
 FROM
   records r
-  JOIN record_contract rc ON rc.record_id = r.id
-  JOIN contracts c ON c.id = rc.contract_id
-  JOIN company c1 ON c1.id = c.party_b_id
-  JOIN project p ON p.company_id = c1.id
-  LEFT JOIN project p2 ON c.project_id = p2.id
+  LEFT JOIN record_contract rc ON rc.record_id = r.id
+  JOIN project p ON p.id = r.in_stock_id
+  JOIN project p2 ON p2.id = r.out_stock_id
+  JOIN company c ON c.roles @> '["associated"]'
+  AND c.id = p.company_id
+  OR c.id = p2.company_id
+  JOIN project p3 ON p3.company_id = c.id
+  AND p3.id = p.id
+  OR p3.id = p2.id
+WHERE
+  rc.id IS NULL
+  AND r.in_stock_id IS NOT NULL
+  AND r.out_stock_id IS NOT NULL
