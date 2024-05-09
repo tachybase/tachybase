eded8f4b696a34eeb8d0e2d35c739058de670105
wjh <wwwjh0710@163.com>
2024-05-09 16:03:59 +0800

    fix: 修改组件创建树形结构时默认为级联组件 (#948)
    Reviewed-on: https://git.daoyoucloud.com/daoyoucloud/tachybase/pulls/948
    Reviewed-by: sealday <zhanglin@daoyoucloud.com>
    Co-authored-by: wjh <wwwjh0710@163.com>
    Co-committed-by: wjh <wwwjh0710@163.com>

--- packages/core/client/src/common/useFieldComponentName.tsx
+++ packages/core/client/src/common/useFieldComponentName.tsx
@@ -2,21 +2,24 @@ import { Field } from '@tachybase/schema';
 import { useField, useFieldSchema } from '@tachybase/schema';
 import { useIsFileField } from '../schema-component/antd/form-item/FormItem.Settings';
 import { useColumnSchema } from '../schema-component/antd/table-v2/Table.Column.Decorator';
-import { useCollectionField } from '../data-source';
+import { useCollectionField, useCollectionManager } from '../data-source';
 
 export function useFieldComponentName(): string {
   const { fieldSchema: tableColumnSchema, collectionField: tableColumnField } = useColumnSchema();
   const field = useField<Field>();
+  const cm = useCollectionManager();
   const isFileField = useIsFileField();
   const schema = useFieldSchema();
   const targetCollectionField = useCollectionField();
+  const targetCollection = cm.getCollection(targetCollectionField?.target);
+  const isTreeCollection = targetCollection?.template === 'tree';
   const fieldSchema = tableColumnSchema || schema;
   const collectionField = tableColumnField || targetCollectionField;
-
   const map = {
     // AssociationField 的 mode 默认值是 Select
-    AssociationField: 'Select',
+    AssociationField: isTreeCollection ? 'Cascader' : 'Select',
   };
+
   const fieldComponentName =
     fieldSchema?.['x-component-props']?.['mode'] ||
     field?.componentProps?.['mode'] ||
