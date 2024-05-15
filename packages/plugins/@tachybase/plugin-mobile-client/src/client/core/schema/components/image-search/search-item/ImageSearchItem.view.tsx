af9e7374137f1d37ac5ba60a69f520e5b932aaad
bai.zixv <bai.zixv@foxmail.com>
2024-05-15 13:33:57 +0800

    fix: plugin-mobile-client, image-search

--- packages/plugins/@tachybase/plugin-mobile-client/src/client/core/schema/components/image-search/search-item/ImageSearchItem.view.tsx
+++ packages/plugins/@tachybase/plugin-mobile-client/src/client/core/schema/components/image-search/search-item/ImageSearchItem.view.tsx
@@ -1,11 +1,13 @@
-import { SortableItem, css, withDynamicSchemaProps } from '@tachybase/client';
+import { SortableItem, css, useCollection, useDesigner, withDynamicSchemaProps } from '@tachybase/client';
+import { useFieldSchema } from '@tachybase/schema';
 import { Image, JumboTabs } from 'antd-mobile';
 import React from 'react';
-import { useActionImageSearchItemView } from '../hooks/useAction.ImageSearchItemView';
+import { useTranslation } from '../../../../../locale';
+// import { useActionImageSearchItemView } from '../hooks/useAction.ImageSearchItemView';
 
 export const ImageSearchItemView = withDynamicSchemaProps(
   (props) => {
-    const { collectionField, Designer, items, onSelect } = useActionImageSearchItemView(props);
+    const { collectionField, Designer, items, onSelect } = useAction(props);
 
     if (!collectionField) {
       return null;
@@ -24,6 +26,63 @@ export const ImageSearchItemView = withDynamicSchemaProps(
   },
   { displayName: 'ImageSearchItemView' },
 );
+
+function useAction(props) {
+  const { list, onSelected, valueKey: _valueKey, labelKey: _labelKey, filterKey } = props;
+  const fieldSchema = useFieldSchema();
+  const collection = useCollection();
+  const { t } = useTranslation();
+  const collectionField = React.useMemo(
+    () => collection?.getField(fieldSchema['fieldName'] as any),
+    [collection, fieldSchema['fieldName']],
+  );
+  const Designer = useDesigner();
+  const valueKey = _valueKey || collectionField?.targetKey || 'id';
+  const labelKey = _labelKey || fieldSchema['x-component-props']?.fieldNames?.label || valueKey;
+
+  const onSelect = (itemKey) => {
+    const key = itemKey.keyPath?.[0] || itemKey;
+    onSelected([key], filterKey);
+  };
+
+  const fieldNames = {
+    title: labelKey || valueKey,
+    key: valueKey,
+  };
+
+  const itemsData = (list || []).map((item) => {
+    const { type, ['image_show']: imageObj, [fieldNames.title]: label, [fieldNames.key]: key } = item;
+
+    // TODO: 需要更好的处理url方式
+    const origin = location?.origin || '';
+    const sourceUrl = imageObj?.[0]?.url;
+    let imageUrl = sourceUrl ?? '';
+    if (!imageUrl.includes('http')) {
+      imageUrl = `${origin}${sourceUrl}`;
+    }
+
+    if (type === 'all') {
+      return {
+        label: t('AllProducts'),
+        key: 'all',
+        imageUrl: imageUrl,
+      };
+    }
+
+    return {
+      label,
+      key,
+      imageUrl: imageUrl,
+    };
+  });
+
+  return {
+    collectionField,
+    Designer,
+    items: itemsData,
+    onSelect,
+  };
+}
 
 const ImageDescription = (props) => {
   const { srcUrl, label } = props;
