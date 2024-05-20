8a421f34ade030bb815def7783f023e447fca972
sealday <zhanglin@daoyoucloud.com>
2024-05-20 23:59:10 +0800

    feat: support comments (#1022)
    Co-authored-by: sealday <sealday@gmail.com>
    Reviewed-on: https://git.daoyoucloud.com/daoyoucloud/tachybase/pulls/1022

---
+++ packages/plugins/@tachybase/plugin-comments/src/locale/en-US.js
@@ -0,0 +1,43 @@
+/**
+ * This file is part of the NocoBase (R) project.
+ * Copyright (c) 2020-2024 NocoBase Co., Ltd.
+ * Authors: NocoBase Team.
+ *
+ * This program is offered under a commercial license.
+ * For more information, see <https://www.nocobase.com/agreement>
+ */
+
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+var en_US_exports = {};
+__export(en_US_exports, {
+  default: () => en_US_default
+});
+module.exports = __toCommonJS(en_US_exports);
+var en_US_default = {
+  "Comment": "Comment",
+  "Comment Collection": "Comment collection",
+  "Comment Content": "Content",
+  "Cancel": "Cancel",
+  "Update Comment": "Update comment",
+  "commented": "commented",
+  "Quote Reply": "Quote reply",
+  "Delete": "Delete",
+  "Edit": "Edit",
+  "Enable Create": "Allow adding comments"
+};
