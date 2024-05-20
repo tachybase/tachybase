abbbf1db94b97a27ca4349475c4feba6ee252663
sealday <zhanglin@daoyoucloud.com>
2024-05-20 23:59:10 +0800

    feat: support comments (#1022)
    Co-authored-by: sealday <sealday@gmail.com>
    Reviewed-on: https://git.daoyoucloud.com/daoyoucloud/tachybase/pulls/1022

---
+++ packages/plugins/@tachybase/plugin-comments/src/locale/zh-CN.js
@@ -0,0 +1,43 @@
+/**
+ * This file is part of the NocoBase (R) project.
+ * Copyright (c) 2020-2024 NocoBase Co., Ltd.
+ * Authors: NocoBase Team.
+ *
+ * This program is offered under a commercial license.
+ * For more information, see <https://www.nocobase.com/agreement>
+ */
+
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+var zh_CN_exports = {};
+__export(zh_CN_exports, {
+  default: () => zh_CN_default
+});
+module.exports = __toCommonJS(zh_CN_exports);
+var zh_CN_default = {
+  "Comment": "\u8BC4\u8BBA",
+  "Comment Collection": "\u8BC4\u8BBA\u8868",
+  "Comment Content": "\u8BC4\u8BBA\u5185\u5BB9",
+  "Cancel": "\u53D6\u6D88",
+  "Update Comment": "\u66F4\u65B0\u8BC4\u8BBA",
+  "commented": "\u8BC4\u8BBA\u4E8E",
+  "Quote Reply": "\u5F15\u7528\u5E76\u56DE\u590D",
+  "Delete": "\u5220\u9664",
+  "Edit": "\u7F16\u8F91",
+  "Enable Create": "\u5141\u8BB8\u589E\u52A0\u8BC4\u8BBA"
+};
