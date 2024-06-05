19fa2d63d9dffe2f94ca79fe9b8d465c9ba81392
sealday <zhanglin@daoyoucloud.com>
2024-06-05 17:20:11 +0800

    chore(version): release v0.21.44 (#1130)
    Reviewed-on: https://git.daoyoucloud.com/daoyoucloud/tachybase/pulls/1130

--- packages/presets/rental/src/server/index.ts
+++ packages/presets/rental/src/server/index.ts
@@ -3,13 +3,20 @@ import PresetTachyBase from '@tachybase/preset-tachybase';
 import _ from 'lodash';
 
 export class PluginRental extends PresetTachyBase {
-  #builtInPlugins = ['approval-mobile', 'core', 'homepage', 'external-data-source'];
+  #builtInPlugins = ['core', 'homepage'];
 
   get builtInPlugins() {
     return super.builtInPlugins.concat(this.#builtInPlugins);
   }
 
-  #localPlugins = ['rental>=0.21.0', 'field-markdown-vditor>=0.21.31', 'comments>=0.21.31'];
+  #localPlugins = [
+    'rental>=0.21.0',
+    'field-markdown-vditor>=0.21.31',
+    'comments>=0.21.31',
+    'workflow-notice>=0.21.43',
+    'approval-mobile>=0.21.43',
+    'external-data-source>=0.21.43',
+  ];
 
   get localPlugins() {
     return super.localPlugins.concat(this.#localPlugins);
