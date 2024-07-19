e9912a00d896e6f3e69cf326cc6861c8e9ff6c9a
bai.zixv <bai.zixv@foxmail.com>
2024-07-19 10:23:14 +0800

    feat: approval, carbon copy unique record (#1349)
    Reviewed-on: https://git.daoyoucloud.com/daoyoucloud/tachybase/pulls/1349
    Co-authored-by: bai.zixv <bai.zixv@foxmail.com>
    Co-committed-by: bai.zixv <bai.zixv@foxmail.com>

--- packages/plugins/@tachybase/plugin-workflow/src/server/features/approval/utils.ts
+++ packages/plugins/@tachybase/plugin-workflow/src/server/features/approval/utils.ts
@@ -18,3 +18,28 @@ export async function jsonParse(expression, scope): Promise<any[]> {
     return [result];
   }
 }
+
+// 根据一系列唯一性字段, 去重对象数组
+// result = [{id:1,name,2,weight:10, time:1}]
+// 用法示例: findUniquifyObjects(result, ['name', 'weight'], 'time', (a, b) => a - b);
+export function findUniqueObjects(
+  sourceArray = [],
+  uniqueByArray: string[],
+  compareByKey = '',
+  compareByFunc = (a, b) => a - b,
+): any[] {
+  const uniqueMap = {};
+  let result = [];
+  sourceArray.forEach((obj) => {
+    const uniqueKey = uniqueByArray.map((field) => obj[field]).join('|');
+    const existingObj = uniqueMap[uniqueKey];
+
+    if (!existingObj || compareByFunc(obj[compareByKey], existingObj[compareByKey]) > 0) {
+      uniqueMap[uniqueKey] = obj;
+    }
+  });
+
+  result = [...Object.values(uniqueMap)];
+
+  return result;
+}
