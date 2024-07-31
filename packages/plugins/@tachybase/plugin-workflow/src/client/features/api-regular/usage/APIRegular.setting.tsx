444984fc0269c1164d995ad4d9175e6bfc34c41e
sealday <zhanglin@daoyoucloud.com>
2024-07-31 14:15:08 +0800

    fix: workflow-trigger (#1413)
    Co-authored-by: sealday <sealday@gmail.com>
    Reviewed-on: https://git.daoyoucloud.com/daoyoucloud/tachybase/pulls/1413

--- packages/plugins/@tachybase/plugin-workflow/src/client/features/api-regular/usage/APIRegular.setting.tsx
+++ packages/plugins/@tachybase/plugin-workflow/src/client/features/api-regular/usage/APIRegular.setting.tsx
@@ -2,13 +2,10 @@ import {
   ActionDesigner,
   SchemaSettings,
   SchemaSettingsItemType,
-  useDesignable,
   useSchemaToolbar,
   WorkflowConfig,
 } from '@tachybase/client';
 import { isValid, useFieldSchema } from '@tachybase/schema';
-
-import { useTranslation } from 'react-i18next';
 
 const schemaSettingsItems: SchemaSettingsItemType[] = [
   {
@@ -17,32 +14,6 @@ const schemaSettingsItems: SchemaSettingsItemType[] = [
     useComponentProps() {
       const { buttonEditorProps } = useSchemaToolbar();
       return buttonEditorProps;
-    },
-  },
-  {
-    name: 'Date',
-    type: 'select',
-    useComponentProps() {
-      const { dn } = useDesignable();
-      const { t } = useTranslation();
-      const fieldSchema = useFieldSchema();
-      return {
-        title: t('Data will be updated'),
-        options: [
-          { label: t('Selected'), value: 'selected' },
-          { label: t('All'), value: 'all' },
-        ],
-        value: fieldSchema['x-action-settings']?.['updateMode'],
-        onChange: (value) => {
-          fieldSchema['x-action-settings']['updateMode'] = value;
-          dn.emit('patch', {
-            schema: {
-              'x-uid': fieldSchema['x-uid'],
-              'x-action-settings': fieldSchema['x-action-settings'],
-            },
-          });
-        },
-      };
     },
   },
   {
