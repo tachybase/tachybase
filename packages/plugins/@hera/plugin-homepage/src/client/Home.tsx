42850b715ea42f74b2b6fdaf98efecd6bfb953fd
wjh <wwwjh0710@163.com>
2024-08-29 16:33:43 +0800

    fix: 更新首页内容 (#1495)
    Reviewed-on: https://git.daoyoucloud.com/daoyoucloud/tachybase/pulls/1495
    Co-authored-by: wjh <wwwjh0710@163.com>
    Co-committed-by: wjh <wwwjh0710@163.com>

--- packages/plugins/@hera/plugin-homepage/src/client/Home.tsx
+++ packages/plugins/@hera/plugin-homepage/src/client/Home.tsx
@@ -1,69 +1,47 @@
 import React from 'react';
-import { useRequest } from '@tachybase/client';
 
-import { Carousel, Image } from 'antd';
+import { Button, Col, Image, Row } from 'antd';
 
 import { useStyles } from './style';
 
 export const HomePage: React.FC<{}> = () => {
   const { styles } = useStyles();
-  const { data, loading } = useRequest<{ data: any }>({
-    url: 'home_page_presentations:list',
-    params: {
-      'appends[]': 'pictures',
-    },
-  });
-
-  if (loading) {
-    return;
-  }
+  const onClick = () => {
+    window.location.href = '/admin';
+  };
   const date = new Date();
   const year = date.getFullYear();
   return (
     <div className={styles.home}>
       <header>
-        <div className="headerStyle">
-          <span className="headerTitle">上海创兴建筑设备租赁有限公司</span>
-          <ul>
-            <li className="active">公司简介</li>
-            <li>新闻中心</li>
-            <li>产品展示</li>
-            <li>工程案例</li>
-            <li>联系我们</li>
-          </ul>
-        </div>
+        <Row className="headerStyle">
+          <Col span={4}>
+            <span className="headerTitle">上海创兴建筑设备租赁有限公司</span>
+          </Col>
+          <Col span={15}>
+            <ul>
+              <li className="active">公司简介</li>
+              <li>新闻中心</li>
+              <li>产品展示</li>
+              <li>工程案例</li>
+              <li>联系我们</li>
+            </ul>
+          </Col>
+          <Col span={5}>
+            <Button type="primary" onClick={onClick}>
+              管理系统入口
+            </Button>
+          </Col>
+        </Row>
       </header>
       <main>
-        <Carousel autoplay>
-          {data?.data?.map((item) => (
-            <div key={item.id}>
-              <Image preview={false} src={item.pictures[0].url} />
-            </div>
-          ))}
-        </Carousel>
+        <Image
+          preview={false}
+          src="https://tachybase-1321007335.cos.ap-shanghai.myqcloud.com/de1b13e4e7f31e4b6c012f9290aff6dc.jpg"
+        />
+        <Button onClick={onClick}>管理系统入口</Button>
       </main>
-      <footer>
-        <ul>
-          <li>
-            <a>公司简介</a>
-          </li>
-          <li>
-            <a>真实合同</a>
-          </li>
-          <li>
-            <a>企业文化</a>
-          </li>
-          <li>
-            <a>联系我们</a>
-          </li>
-          <li>
-            <a href="/admin">管理系统入口</a>
-          </li>
-        </ul>
-        <div>
-          <span>©2023-{year} 上海道有云网络科技有限公司 版权所有 沪ICP备2023024678号</span>
-        </div>
-      </footer>
+      <footer>©2023-{year} 上海道有云网络科技有限公司 版权所有 沪ICP备2023024678号</footer>
     </div>
   );
 };
