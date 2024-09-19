ab1c4e5bceee3101d8e2af1ee3742b16ff9e2198
sealday <zhanglin@daoyoucloud.com>
2024-09-19 15:08:25 +0800

    feat: 订阅渠道管理 (#1546)
    Co-authored-by: sealday <sealday@gmail.com>
    Reviewed-on: https://git.daoyoucloud.com/daoyoucloud/tachybase/pulls/1546

--- packages/plugins/@tachybase/plugin-messages/src/client/index.tsx
+++ packages/plugins/@tachybase/plugin-messages/src/client/index.tsx
@@ -1,20 +1,52 @@
 import { Plugin } from '@tachybase/client';
 
+import { lang } from './locale';
+import { SubscriptionManager } from './SubscriptionManager';
+
+interface MessageType {
+  title: string;
+  name: string;
+}
+
 export class PluginMessagesClient extends Plugin {
+  private _messageTypes: MessageType[] = [];
+
   async afterAdd() {
     // await this.app.pm.add()
   }
 
   async beforeLoad() {}
 
+  get messageTypes() {
+    return this._messageTypes;
+  }
+
+  registe(messageType: MessageType) {
+    const i = this._messageTypes.findIndex((type) => messageType.name === type.name);
+    if (i === -1) {
+      this._messageTypes.push(messageType);
+    } else {
+      this._messageTypes[i] = messageType;
+    }
+  }
+
   // You can get and modify the app instance here
   async load() {
-    console.log(this.app);
-    // this.app.addComponents({})
-    // this.app.addScopes({})
-    // this.app.addProvider()
-    // this.app.addProviders()
-    // this.app.router.add()
+    this.userSettingsManager.add('sub-manager', {
+      title: lang('Subscription management'),
+      icon: 'BellOutlined',
+      Component: SubscriptionManager,
+    });
+
+    this.registe({
+      name: 'browser',
+      title: lang('Browser notification'),
+    });
+
+    this.registe({
+      name: 'sms',
+      title: lang('SMS notification'),
+    });
   }
 }
 
