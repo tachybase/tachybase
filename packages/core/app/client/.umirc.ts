101a02fbea2fe9618c8049d1da22387473615081
bai.zixv <bai.zixv@foxmail.com>
2024-10-31 19:09:53 +0800

    Revert "fix(tb): 修复平板设备过于缩小问题" (#1670)
    This reverts commit 54a04f9dfbc80f37ef758417c63e7a1775dab148.
    
    Reviewed-on: https://git.daoyoucloud.com/tachybase/tachybase/pulls/1670

--- packages/core/app/client/.umirc.ts
+++ packages/core/app/client/.umirc.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   title: 'Loading...',
   devtool: process.env.NODE_ENV === 'development' ? 'source-map' : false,
   favicons: [`${appPublicPath}favicon/favicon.ico`],
-  metas: [{ name: 'viewport', content: 'initial-scale=1' }],
+  metas: [{ name: 'viewport', content: 'initial-scale=0.1' }],
   links: [
     { rel: 'apple-touch-icon', size: '180x180', ref: `${appPublicPath}favicon/apple-touch-icon.png` },
     { rel: 'icon', type: 'image/png', size: '32x32', ref: `${appPublicPath}favicon/favicon-32x32.png` },
