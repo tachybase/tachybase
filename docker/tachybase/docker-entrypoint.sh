694de84cc448cc44c0aca86b1cf22fb2d66e0e13
sealday <zhanglin@daoyoucloud.com>
2024-11-04 03:47:17 +0800

    fix: docker-entrypoint (#1685)
    Co-authored-by: sealday <sealday@gmail.com>
    Reviewed-on: https://git.daoyoucloud.com/tachybase/tachybase/pulls/1685

--- docker/tachybase/docker-entrypoint.sh
+++ docker/tachybase/docker-entrypoint.sh
@@ -12,7 +12,7 @@ output_file="/app/public/index.html"
 # The implementation depends on index.html.
 # If you modify index.html, please pay attention to whether this logic is affected.
 {
-    if [[ -f "$sentry_part" ]]; then
+    if [ -f "$sentry_part" ]; then
       sentry_script=$(<"$sentry_part")
       cat "$original_file" | sed "/<link rel=\"stylesheet\" href=\"\/global.css\">/a $sentry_script"
     else
