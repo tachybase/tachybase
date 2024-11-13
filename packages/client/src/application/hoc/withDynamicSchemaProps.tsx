d6d769eae157ee89e96d691af5f8c09d88d6ed83
sealday <zhanglin@daoyoucloud.com>
2024-11-13 21:18:04 +0800

    feat: dynamic schema props decorator (#1742)
    Co-authored-by: sealday <sealday@gmail.com>
    Reviewed-on: https://git.daoyoucloud.com/tachybase/tachybase/pulls/1742

--- packages/client/src/application/hoc/withDynamicSchemaProps.tsx
+++ packages/client/src/application/hoc/withDynamicSchemaProps.tsx
@@ -76,3 +76,71 @@ export function withDynamicSchemaProps<T = any>(Component: any, options: WithSch
 
   return ComponentWithProps;
 }
+
+export function DynamicSchemaProps(options: WithSchemaHookOptions = {}) {
+  return <T = any,>(Component: any, _context: any) => {
+    const displayName = options.displayName || Component.displayName || Component.name;
+    const ComponentWithProps: ComponentType<T> = (props) => {
+      const { dn, findComponent } = useDesignable();
+      const scope = useExpressionScope();
+      const useComponentPropsStr = useMemo(() => {
+        const xComponent = dn.getSchemaAttribute('x-component');
+        const xDecorator = dn.getSchemaAttribute('x-decorator');
+        const xUseComponentProps = dn.getSchemaAttribute('x-use-component-props');
+        const xUseDecoratorProps = dn.getSchemaAttribute('x-use-decorator-props');
+
+        if (xComponent && xUseComponentProps && findComponent(xComponent) === ComponentWithProps) {
+          return xUseComponentProps;
+        }
+
+        if (xDecorator && xUseDecoratorProps && findComponent(xDecorator) === ComponentWithProps) {
+          return xUseDecoratorProps;
+        }
+      }, [dn]);
+      const useSchemaProps = useMemo(() => {
+        const getHook = (str: string, scope: Record<string, any>, allText: string) => {
+          let res = undefined;
+          if (_.isFunction(str)) {
+            res = str;
+          } else {
+            res = scope[str];
+            if (!res) {
+              console.error(`${allText} is not registered`);
+            }
+          }
+          return res || useDefaultSchemaProps;
+        };
+
+        if (!useComponentPropsStr) {
+          return useDefaultSchemaProps;
+        }
+
+        if (_.isFunction(useComponentPropsStr)) {
+          return useComponentPropsStr;
+        }
+
+        const pathList = useComponentPropsStr.split('.');
+        let result;
+
+        for (const item of pathList) {
+          result = getHook(item, result || scope, useComponentPropsStr);
+        }
+
+        return result;
+      }, [scope, useComponentPropsStr]);
+      const schemaProps = useSchemaProps(props);
+
+      const memoProps = useMemo(() => {
+        return { ...props, ...schemaProps };
+      }, [schemaProps, props]);
+
+      // @ts-ignore
+      return <Component {...memoProps}>{props.children}</Component>;
+    };
+
+    Component.displayName = displayName;
+    ComponentWithProps.displayName = `withSchemaProps(${displayName})`;
+
+    return ComponentWithProps;
+  };
+}
