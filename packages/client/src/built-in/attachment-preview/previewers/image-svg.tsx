e2da0d4e3399883976529fcab5d07a6098476465
wjh <wwwjh0710@163.com>
2024-11-20 16:38:38 +0800

    fix: adjust SVG component (#1799)
    Reviewed-on: https://git.daoyoucloud.com/tachybase/tachybase/pulls/1799
    Reviewed-by: sealday <sealday@gmail.com>
    Co-authored-by: wjh <wwwjh0710@163.com>
    Co-committed-by: wjh <wwwjh0710@163.com>

--- packages/client/src/built-in/attachment-preview/previewers/image-svg.tsx
+++ packages/client/src/built-in/attachment-preview/previewers/image-svg.tsx
@@ -1,45 +1,14 @@
 import React from 'react';
 
-import { css } from '@emotion/css';
-import { ReactSVG } from 'react-svg';
-
 export const imageSvg = {
   key: 'image/svg+xml',
   type: 'image/svg+xml',
   viewComponet: (props) => {
     const { file, prefixCls } = props;
-    return (
-      file.imageUrl && (
-        <ReactSVG
-          src={`${file.url}`}
-          className={`${prefixCls}-list-item-image ${css`
-            svg {
-              width: 100%;
-              height: 100%;
-            }
-          `}`}
-        />
-      )
-    );
+    return file.imageUrl && <img src={`${file.url}`} />;
   },
   checkedComponent: (props) => {
     const { file } = props;
-    return (
-      file.imageUrl && (
-        <ReactSVG
-          {...props}
-          src={`${file.url}`}
-          className={`${css`
-            svg {
-              width: 90%;
-              height: auto;
-              display: flex;
-              justify-content: center;
-              align-items: center;
-            }
-          `}`}
-        />
-      )
-    );
+    return file.imageUrl && <img {...props} src={`${file.url}`} />;
   },
 };
