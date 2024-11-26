71691de4cbd8869092d92e1592de8aee80102896
sealday <sealday@gmail.com>
2024-11-27 02:53:38 +0800

    feat: cloud component editors

--- packages/module-prototype-cloud-component/src/client/index.tsx
+++ packages/module-prototype-cloud-component/src/client/index.tsx
@@ -1,10 +1,22 @@
 import { Plugin } from '@tachybase/client';
 
 import LibEditor from './component-editor/LibEditor';
+import { CloudComponentManager } from './manager/CloudComponentManager';
 
 export class PluginCloudComponentClient extends Plugin {
   async load() {
-    this.app.systemSettingsManager.add('cloud-component-edit', {
+    this.app.systemSettingsManager.add('cloud-component', {
+      title: 'Cloud component',
+      icon: 'deploymentunitoutlined',
+      sort: 201,
+    });
+    this.app.systemSettingsManager.add('cloud-component.main', {
+      title: 'Cloud component',
+      icon: 'deploymentunitoutlined',
+      Component: CloudComponentManager,
+      sort: 201,
+    });
+    this.app.systemSettingsManager.add('cloud-component.edit', {
       title: 'Cloud component',
       icon: 'deploymentunitoutlined',
       Component: LibEditor,
---
+++ packages/module-prototype-cloud-component/src/client/manager/CloudComponentManager.fields.ts
@@ -0,0 +1,40 @@
+export const fieldsets = {
+  name: {
+    type: 'string',
+    'x-toolbar': 'FormItemSchemaToolbar',
+    'x-settings': 'fieldSettings:FormItem',
+    'x-component': 'CollectionField',
+    'x-decorator': 'FormItem',
+    'x-collection-field': 'cloudComponents.name',
+    'x-component-props': {},
+  },
+  code: {
+    type: 'string',
+    'x-toolbar': 'FormItemSchemaToolbar',
+    'x-settings': 'fieldSettings:FormItem',
+    'x-component': 'CollectionField',
+    'x-decorator': 'FormItem',
+    'x-collection-field': 'cloudComponents.code',
+    'x-component-props': {},
+  },
+
+  data: {
+    type: 'string',
+    'x-toolbar': 'FormItemSchemaToolbar',
+    'x-settings': 'fieldSettings:FormItem',
+    'x-component': 'CollectionField',
+    'x-decorator': 'FormItem',
+    'x-collection-field': 'cloudComponents.data',
+    'x-component-props': {},
+  },
+
+  description: {
+    type: 'string',
+    'x-toolbar': 'FormItemSchemaToolbar',
+    'x-settings': 'fieldSettings:FormItem',
+    'x-component': 'CollectionField',
+    'x-decorator': 'FormItem',
+    'x-collection-field': 'cloudComponents.description',
+    'x-component-props': {},
+  },
+};
