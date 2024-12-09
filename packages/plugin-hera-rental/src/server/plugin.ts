f4f745ecf270efe74ee575d86e97ffbf2b952d81
wjh <wwwjh0710@163.com>
2024-12-09 22:49:11 +0800

    fix: migration of hera hook to workflow (#1882)
    Reviewed-on: https://git.daoyoucloud.com/tachybase/tachybase/pulls/1882
    Co-authored-by: wjh <wwwjh0710@163.com>
    Co-committed-by: wjh <wwwjh0710@163.com>

--- packages/plugin-hera-rental/src/server/plugin.ts
+++ packages/plugin-hera-rental/src/server/plugin.ts
@@ -5,23 +5,12 @@ import { Container } from '@tachybase/utils';
 
 import { SqlLoader } from '@hera/plugin-core';
 
-import { ContractRuleService } from './services/contract-rule-service';
-import { ContractService } from './services/contract-service';
-import { DetailCheckService } from './services/detail-check-service';
-import { ProjectService } from './services/project-service';
-import { RecordService } from './services/record-service';
-
 export class PluginRentalServer extends Plugin {
   cache: Cache;
 
   async load() {
     const sqlLoader = Container.get(SqlLoader);
     await sqlLoader.loadSqlFiles(path.join(__dirname, './sqls'));
-    Container.get(RecordService).load();
-    Container.get(ContractRuleService).load();
-    Container.get(ProjectService).load();
-    Container.get(DetailCheckService).load();
-    Container.get(ContractService).load();
 
     this.cache = await this.app.cacheManager.createCache({
       name: '@hera/plugin-rental',
