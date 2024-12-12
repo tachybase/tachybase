68ec026cdfe610408f23c990ea393dbaef5c7cb6
Toby <2287769986@qq.com>
2024-12-12 17:36:54 +0800

    fix: extern pg not show uppercase table (#1893)
    Reviewed-on: https://git.daoyoucloud.com/tachybase/tachybase/pulls/1893
    Reviewed-by: sealday <sealday@gmail.com>
    Co-authored-by: Toby <2287769986@qq.com>
    Co-committed-by: Toby <2287769986@qq.com>

--- packages/plugin-data-source-common/src/server/pg/utils.ts
+++ packages/plugin-data-source-common/src/server/pg/utils.ts
@@ -15,7 +15,7 @@ JOIN information_schema.key_column_usage AS kcu
   ON tc.constraint_name = kcu.constraint_name
   AND tc.table_schema = kcu.table_schema
 WHERE tc.constraint_type = 'UNIQUE'
-  AND tc.table_name = '${tableInfo2.tableName}'
+  AND tc.table_name = '"${tableInfo2.tableName}"'
   AND tc.table_schema = '${tableInfo2.schema || 'public'}'
 ORDER BY tc.constraint_name, kcu.column_name;
   `;
