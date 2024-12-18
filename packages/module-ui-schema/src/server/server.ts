3486183a497e95cf836402bbfd34d1dbdfc4cdb5
wildworker <31757692+wildworker@users.noreply.github.com>
2024-12-18 14:39:21 +0800

    fix: uiSchema remove action happened to encounter CRUD operations (#45)
    * fix: uiSchema remove action happened to encounter CRUD operations
    
    * fix: prevert default remove
    
    * Revert "fix: prevert default remove"
    
    This reverts commit 1d65d4ae649580981d5a3235fb768a23575ecf6c.
    
    * Revert "fix: uiSchema remove action happened to encounter CRUD operations"
    
    This reverts commit cc36d5922884d795450379c27805aeebfdc0a35c.
    
    * fix: use middleware to protect uiSchemas remove
    
    * perf: enhance error details for better troubleshooting

--- packages/module-ui-schema/src/server/server.ts
+++ packages/module-ui-schema/src/server/server.ts
@@ -79,6 +79,32 @@ export class ModuleUiSchema extends Plugin {
       });
     });
 
+    this.app.resourcer.use(
+      async (ctx, next) => {
+        const { resourceName, actionName } = ctx.action.params;
+        if (resourceName === 'uiSchemas' && actionName === 'remove') {
+          const skip = await ctx.app.acl.allowManager.isAllowed(resourceName, actionName, ctx);
+          if (skip) {
+            return next();
+          }
+          const role = ctx?.state?.currentRole;
+          if (!role) {
+            ctx.throw(403, 'No Permission: Role not found');
+          }
+          const aclRole = ctx.app.acl.roles.get(role);
+          if (!aclRole) {
+            ctx.throw(403, 'No Permission: Role not found');
+          }
+          const snippetAllowed = aclRole.snippetAllowed(`${resourceName}:${actionName}`);
+          if (!snippetAllowed) {
+            ctx.throw(403, 'No Permission: Role lacks remove permission for uiSchemas');
+          }
+        }
+        await next();
+      },
+      { tag: 'intercept-ui-schema-remove', after: 'acl' },
+    );
+
     this.app.resourcer.define({
       name: 'uiSchemas',
       actions: uiSchemaActions,
