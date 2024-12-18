004f7decab723eaf2ed05353ad89da8325e74210
bai.zixv <bai.zixv@foxmail.com>
2024-12-18 17:27:20 +0800

    feat: some additional

--- packages/module-message/src/client/base/channels/kit.ts
+++ packages/module-message/src/client/base/channels/kit.ts
@@ -1,5 +1,6 @@
 import { Plugin } from '@tachybase/client';
 
+import { KitBrowserChannel } from './browser/kit';
 import { MessageChannelProvider } from './MessageChannel.provider';
 import { KitSiteMessageChannel } from './site-message/kit';
 import { KitSMSChannel } from './sms/kit';
@@ -8,6 +9,7 @@ export class KitChannels extends Plugin {
   async afterAdd() {
     await this.app.pm.add(KitSiteMessageChannel);
     await this.app.pm.add(KitSMSChannel);
+    await this.app.pm.add(KitBrowserChannel);
   }
 
   async load() {
