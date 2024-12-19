6d8f8d283d93b3f77729ae03c913100cc486d52d
sealday <sealday@gmail.com>
2024-12-19 14:59:15 +0800

    revert: index.ts

--- apps/app-rs/src/pages/.plugins/index.ts
+++ apps/app-rs/src/pages/.plugins/index.ts
@@ -1,11 +1,17 @@
+import * as others from '../../../../../packages/module-pdf/src/client';
 import packageMap from './packageMap.json';
 
 type PackageName = keyof typeof packageMap;
 
 async function devDynamicImport(packageName: PackageName) {
+  console.log('🚀 ~ file: index.ts:10 ~ devDynamicImport ~ packageName:', packageName);
   const fileName = packageMap[packageName];
   if (!fileName) {
     return Promise.resolve(null);
+  }
+  // FIXME: 需要处理这个模块的加载问题
+  if (packageName === '@tachybase/module-pdf') {
+    return { ...others };
   }
   return await import(`./packages/${fileName}`);
 }
