080554cf5eb50643e71204b9cb160695cea0a571
bai.zixv <bai.zixv@foxmail.com>
2024-12-20 11:57:33 +0800

    feat: hide or show scroll area logic for context menu

--- packages/client/src/built-in/context-menu/ContextMenu.provider.tsx
+++ packages/client/src/built-in/context-menu/ContextMenu.provider.tsx
@@ -10,15 +10,24 @@ export const ContextMenuProvider = ({ children }) => {
   const [enable, setEnable] = useLocalStorageState<boolean>('context-menu-enabled', {
     defaultValue: true,
   });
+  const [hiddenScrollArea, setHiddenScrollArea] = useLocalStorageState<boolean>('hidden-scroll-area', {
+    defaultValue: false,
+  });
+
   const contextItems = useApp().pluginContextMenu.get();
   const [position, setPosition] = useState({ x: 0, y: 0 });
   const items: MenuProps['items'] = [];
 
-  // XXX: 是否有办法将这个计算缓存, 以及下边那个遍历缓存.
   const contextItemsSorted = Object.values(contextItems).sort((a, b) => (a.sort || 0) - (b.sort || 0));
 
   contextItemsSorted.forEach((item) => {
-    const { actionProps, title, icon } = item.useLoadMethod({ enable, setEnable, position });
+    const { actionProps, title, icon } = item.useLoadMethod({
+      enable,
+      setEnable,
+      hiddenScrollArea,
+      setHiddenScrollArea,
+      position,
+    });
     items.push({
       label: title,
       key: title,
@@ -32,6 +41,8 @@ export const ContextMenuProvider = ({ children }) => {
       value={{
         contextMenuEnabled: enable,
         setContextMenuEnable: setEnable,
+        hiddenScrollArea,
+        setHiddenScrollArea,
         position,
       }}
     >
