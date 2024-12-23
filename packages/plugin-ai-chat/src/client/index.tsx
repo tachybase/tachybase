db51f7c9e8df724c2b26ef63afe79b7c24baae8a
WinC159 <1637375142@qq.com>
2024-12-23 19:20:34 +0800

    feat: aichat cardization

--- packages/plugin-ai-chat/src/client/index.tsx
+++ packages/plugin-ai-chat/src/client/index.tsx
@@ -1,6 +1,9 @@
 import { Plugin } from '@tachybase/client';
 
 import { AIchatModalProvider } from './ai-chat/AIchatModalProvider';
+import { AIchatBlockSettings } from './aichat-card/AIchat-BlockSettings';
+import { AiChatBlock, AIchatBlockInitializer } from './aichat-card/AIchatBlockInitializer';
+import { AIchatSettingsPane } from './aichat-setting/AIchatSettingsShortcut';
 import { AIchatProvider } from './AIchatProvider';
 
 export class PluginAiChatClient extends Plugin {
@@ -14,11 +17,23 @@ export class PluginAiChatClient extends Plugin {
   async load() {
     this.app.use(AIchatModalProvider);
     this.app.use(AIchatProvider);
-    // this.app.addComponents({})
-    // this.app.addScopes({})
-    // this.app.addProvider()
-    // this.app.addProviders()
-    // this.app.router.add()
+    this.app.systemSettingsManager.add('system-services.ai-settings', {
+      icon: 'SettingOutlined',
+      title: '{{t("AIchat settings")}}',
+      Component: AIchatSettingsPane,
+    });
+    this.schemaSettingsManager.add(AIchatBlockSettings);
+    this.app.addComponents({ AiChatBlock });
+    // this.app.schemaInitializerManager.addItem(
+    //   "page:addBlock",
+    //   `otherBlocks.aiChatBlock`,
+    //   AIchatBlockInitializer
+    // ); // 注册到卡片生成器中
+    const blockInitializers = this.app.schemaInitializerManager.get('page:addBlock');
+    blockInitializers?.add('otherBlocks.aiChatBlock', {
+      title: '{{t("AIchat")}}',
+      Component: AIchatBlockInitializer,
+    });
   }
 }
 
