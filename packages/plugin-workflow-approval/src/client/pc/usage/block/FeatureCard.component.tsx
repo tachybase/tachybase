b9c2e9bdebb7fd63e99e828f1a9fd20c0a8973a7
bai.zixv <bai.zixv@foxmail.com>
2024-12-24 14:41:20 +0800

    feat: feature card style

--- packages/plugin-workflow-approval/src/client/pc/usage/block/FeatureCard.component.tsx
+++ packages/plugin-workflow-approval/src/client/pc/usage/block/FeatureCard.component.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
+import { Icon } from '@tachybase/client';
+
+import { useStyles } from './FeatureCard.style';
 
 export const FeatureCard = (props) => {
   const { data } = props;
-  return <div>FeatureCard</div>;
+  const { title, icon = 'AppstoreOutlined', color = '#e5e5e5' } = data || {};
+  const { styles } = useStyles();
+  return (
+    <div className={styles.featureCard}>
+      <div className="icon-wrapper" style={{ backgroundColor: color }}>
+        <Icon className="icon" type={icon} style={{ color }} />
+      </div>
+      <div className="title">{title}</div>
+    </div>
+  );
 };
--- packages/plugin-workflow-approval/src/client/pc/usage/block/FeatureList.style.ts
+++ packages/plugin-workflow-approval/src/client/pc/usage/block/FeatureList.style.ts
@@ -4,9 +4,10 @@ export const useStyles = createStyles(({ css, token }) => {
   return {
     featureList: css`
       display: grid;
-      grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
-      justify-items: center;
+      grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+      justify-items: start;
       gap: 10px;
+      grid-row-gap: 30px;
     `,
   };
 });
