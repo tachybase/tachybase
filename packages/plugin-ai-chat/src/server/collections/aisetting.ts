0db8f173860572d3cf91ad3ae6df4265b56a23c9
WinC159 <1637375142@qq.com>
2024-12-25 18:02:02 +0800

    feat: encrypt ai api key in database

--- packages/plugin-ai-chat/src/server/collections/aisetting.ts
+++ packages/plugin-ai-chat/src/server/collections/aisetting.ts
@@ -1,5 +1,7 @@
 import { CollectionOptions } from '@tachybase/database';
 
+import { decryptSync, encryptSync } from '../util';
+
 export default {
   name: 'aisettings',
   shared: true,
@@ -19,6 +21,13 @@ export default {
     {
       type: 'string',
       name: 'AI_API_KEY',
+      get() {
+        const key = decryptSync(this.dataValues['AI_API_KEY'], '0123456789123456');
+        return key;
+      },
+      set(val: string) {
+        this.dataValues['AI_API_KEY'] = encryptSync(val, '0123456789123456');
+      },
     },
   ],
 } as CollectionOptions;
