fabe0bb9bf6a272293cd355963c0cd41638e245e
Toby <2287769986@qq.com>
2024-12-26 16:32:51 +0800

    fix: ai chat snippets error

--- packages/plugin-ai-chat/src/server/plugin.ts
+++ packages/plugin-ai-chat/src/server/plugin.ts
@@ -22,7 +22,7 @@ export class PluginAiChatServer extends Plugin {
     });
     this.app.acl.registerSnippet({
       // name: `pm.${this.name}.aichat`,
-      name: `pm.system-services.${this.name}`,
+      name: `pm.system-services.ai-settings`,
       actions: ['aichat:*'],
     });
   }
