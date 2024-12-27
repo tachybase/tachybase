fd2f49886b199777d4b6f8137a6ee203d9199304
Toby <2287769986@qq.com>
2024-12-27 19:45:11 +0800

    fix: edit field error

--- packages/plugin-data-source-common/src/client/features/rest-api/collection-edit/EditCollectionAction.component.tsx
+++ packages/plugin-data-source-common/src/client/features/rest-api/collection-edit/EditCollectionAction.component.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { ActionContextProvider, RecordProvider } from '@tachybase/client';
+import { ActionContextProvider, RecordProvider, useAPIClient } from '@tachybase/client';
 
 import { lang, useTranslation } from '../../../locale';
 import { ViewEditCollectionForm } from './EditCollectionForm.view';
@@ -8,12 +8,24 @@ export const EditCollectionAction = (props) => {
   const { t } = useTranslation();
   const { scope, getContainer, item } = props;
   const [visible, setVisible] = useState(false);
+  const [currentRecord, setCurrentRecord] = useState(null);
+
+  const apiClient = useAPIClient();
+  const repo = apiClient.resource('dataSources.collections', item.dataSourceKey);
 
   return (
     <RecordProvider record={item}>
       <ActionContextProvider value={{ visible, setVisible }}>
-        <a onClick={() => setVisible(true)}>{lang('Edit')}</a>
-        <ViewEditCollectionForm scope={scope} item={item} getContainer={getContainer} />
+        <a
+          onClick={async () => {
+            const collectionRecord = await repo.get({ filterByTk: item.key, appends: ['fields'] });
+            setCurrentRecord(collectionRecord?.data?.data);
+            setVisible(true);
+          }}
+        >
+          {lang('Edit')}
+        </a>
+        <ViewEditCollectionForm scope={scope} item={currentRecord} getContainer={getContainer} />
       </ActionContextProvider>
     </RecordProvider>
   );
