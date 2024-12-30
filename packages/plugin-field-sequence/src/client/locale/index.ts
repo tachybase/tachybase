b53ce2ebf003ad12d79f859793410c47fdb4cffe
sealday <sealday@gmail.com>
2024-12-30 11:58:17 +0800

    fix(field-sequence): date format cannot config

--- packages/plugin-field-sequence/src/client/locale/index.ts
+++ packages/plugin-field-sequence/src/client/locale/index.ts
@@ -1,15 +1,17 @@
-import { i18n } from '@tachybase/client';
+import { i18n, tval as nTval } from '@tachybase/client';
 
 import { useTranslation } from 'react-i18next';
 
 export const NAMESPACE = 'field-sequence';
 
-// i18n.addResources('zh-CN', NAMESPACE, zhCN);
-
 export function lang(key: string, options = {}) {
-  return i18n.t(key, { ...options, ns: NAMESPACE });
+  return i18n.t(key, { ...options, ns: [NAMESPACE, 'core'] });
 }
 
 export function usePluginTranslation() {
-  return useTranslation(NAMESPACE);
+  return useTranslation([NAMESPACE, 'core']);
+}
+
+export function tval(str: string) {
+  return nTval(str, [NAMESPACE, 'core']);
 }
