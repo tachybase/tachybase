7d182e5f279dca867f37c79cefb0b5dc2360a5ef
Toby <2287769986@qq.com>
2024-12-30 15:18:11 +0800

    fix: migration enable department

---
+++ packages/module-hera/src/server/migrations/20241230150713-add-module-department.ts
@@ -0,0 +1,24 @@
+import { Migration } from '@tachybase/server';
+
+import PluginCoreServer from '../plugin';
+
+export default class extends Migration {
+  on = 'afterLoad'; // 'beforeLoad' or 'afterLoad'
+  appVersion = '<0.23.23';
+
+  async up() {
+    const plugin = this.app.pm.get(PluginCoreServer);
+    if (!plugin.enabled) {
+      return;
+    }
+    // 开启hera的也必须开启department
+    await this.pm.repository.update({
+      filter: {
+        packageName: '@tachybase/module-department',
+      },
+      values: {
+        enbaled: true,
+      },
+    });
+  }
+}
