e7fb232a19dadbc1bae380a6290314a9b1d44b1f
Toby <2287769986@qq.com>
2025-01-03 15:10:30 +0800

    fix: allowNewMenu sometimes not work

--- packages/module-ui-schema/src/server/server-hooks/hooks/bind-menu-to-role.ts
+++ packages/module-ui-schema/src/server/server-hooks/hooks/bind-menu-to-role.ts
@@ -6,9 +6,19 @@ export async function bindMenuToRole({ schemaInstance, db, options }) {
     },
   });
 
+  const xUid = schemaInstance.get('x-uid');
   for (const role of addNewMenuRoles) {
+    // 此处由于可能开启allowNewMenu之前父节点没给到角色,导致角色没有权限,所以这里需要给角色添加所有父节点权限
+    const ancestors = await db.getRepository('uiSchemaTreePath').find({
+      fields: ['ancestor'],
+      filter: {
+        descendant: schemaInstance.get('x-uid'),
+      },
+    });
+    const ancestorsIds = ancestors.map((item) => item.get('ancestor'));
+    ancestorsIds.push(xUid);
     await db.getRepository('roles.menuUiSchemas', role.get('name')).add({
-      tk: schemaInstance.get('x-uid'),
+      tk: ancestorsIds,
       transaction,
     });
   }
