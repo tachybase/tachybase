59f202ef30baffabb7027558c35be65af5b5f079
bai.zixv <602250248@qq.com>
2025-01-08 18:09:09 +0800

    feat: menu, sub-menu

--- packages/client/src/schema-component/antd/menu/Menu.styles.ts
+++ packages/client/src/schema-component/antd/menu/Menu.styles.ts
@@ -4,7 +4,11 @@ export const useStyles = createStyles(({ css, token }) => {
   return {
     subMenuDesignerCss: css`
       position: relative;
-      display: inline-block;
+      display: flex;
+      flex-direction: row;
+      flex-wrap: nowrap;
+      justify-content: flex-start;
+      align-items: center;
       margin-left: -24px;
       margin-right: -34px;
       padding: 0 34px 0 24px;
@@ -15,6 +19,12 @@ export const useStyles = createStyles(({ css, token }) => {
       &:hover {
         > .general-schema-designer {
           display: block;
+          visibility: visible;
+        }
+        .ant-space-item:hover {
+          border-radius: 5px;
+          background-color: var(--colorSettings);
+          color: #fff;
         }
       }
       &.tb-action-link {
@@ -26,29 +36,14 @@ export const useStyles = createStyles(({ css, token }) => {
         }
       }
       > .general-schema-designer {
-        position: absolute;
-        z-index: 999;
-        top: 0;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        display: none;
-        background: var(--colorBgSettingsHover);
-        border: 0;
+        visibility: hidden;
         pointer-events: none;
         > .general-schema-designer-icons {
-          position: absolute;
-          right: 2px;
-          top: 2px;
-          line-height: 16px;
           pointer-events: all;
           .ant-space-item {
-            background-color: var(--colorSettings);
-            color: #fff;
             line-height: 16px;
             width: 16px;
             padding-left: 1px;
-            align-self: stretch;
           }
         }
       }
