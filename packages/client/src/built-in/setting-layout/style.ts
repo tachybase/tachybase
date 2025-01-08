c36620498612a2c3e74c9274d9cf061740d69fdd
bai.zixv <602250248@qq.com>
2025-01-08 13:34:48 +0800

    feat: note

--- packages/client/src/built-in/setting-layout/style.ts
+++ packages/client/src/built-in/setting-layout/style.ts
@@ -3,6 +3,7 @@ import { createStyles } from 'antd-style';
 export const useStyles = createStyles(({ css, token }) => {
   return {
     settingLayout: css`
+      /* Depends on antd's ProLayout component structure */
       .ant-pro-global-header-logo {
         cursor: pointer;
       }
