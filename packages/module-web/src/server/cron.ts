1dde734fc41848d6eaf7b4c6522b1b539b82dd20
Toby <2287769986@qq.com>
2025-01-09 19:37:00 +0800

    fix: cron locale cache empty

--- packages/module-web/src/server/cron.ts
+++ packages/module-web/src/server/cron.ts
@@ -3,7 +3,7 @@ import { requireModule } from '@tachybase/utils';
 
 export const getCronLocale = (lang: string) => {
   const lng = lang.replace('-', '_');
-  const files = [resolve(__dirname, `./../locale/cron/${lng}`)];
+  const files = [resolve(__dirname, `./../../../client/src/locale/cron/${lng}`)];
   if (process.env.APP_ENV !== 'production') {
     files.push(`@tachybase/client/src/locale/cron/${lng}`, `@tachybase/client/lib/locale/cron/${lng}`);
   }
