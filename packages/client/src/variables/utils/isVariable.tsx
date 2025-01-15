f9835f0b7795b25fb375b02cbf8a2d07eb089572
Toby <2287769986@qq.com>
2025-01-15 15:36:22 +0800

    fix: clear code

--- packages/client/src/variables/utils/isVariable.tsx
+++ packages/client/src/variables/utils/isVariable.tsx
@@ -1,5 +1,4 @@
-export const REGEX_OF_VARIABLE = /^\s*\{\{\s*([a-zA-Z0-9_$-.]+?)\s*\}\}\s*$/g;
-export const REGEX_OF_VARIABLE_IN_EXPRESSION = /\{\{\s*([a-zA-Z0-9_$-.]+?)\s*\}\}/g;
+export const REGEX_OF_VARIABLE = /\{\{\s*([a-zA-Z0-9_$-.]+?)\s*\}\}/g;
 
 export const isVariable = (str: unknown) => {
   if (typeof str !== 'string') {
@@ -12,12 +11,4 @@ export const isVariable = (str: unknown) => {
   }
 
   return true;
-};
-
-export const getVariablesFromExpression = (str: string) => {
-  const matches = str.match(REGEX_OF_VARIABLE_IN_EXPRESSION);
-  if (!matches) {
-    return [];
-  }
-  return matches;
 };
