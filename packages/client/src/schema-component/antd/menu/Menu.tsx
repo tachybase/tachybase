a3aceb0fc9cc7f49b588ac1605b2a184d35dab3c
bai.zixv <bai.zixv@foxmail.com>
2025-01-16 18:28:11 +0800

    feat: menu, drag distance

--- packages/client/src/schema-component/antd/menu/Menu.tsx
+++ packages/client/src/schema-component/antd/menu/Menu.tsx
@@ -306,9 +306,7 @@ export const Menu: ComposedMenu = observer(
     const sensors = useSensors(
       useSensor(PointerSensor, {
         activationConstraint: {
-          distance: 1, // 移动 1px 后触发拖拽
-          delay: 300,
-          tolerance: 5, // 允许的移动距离
+          distance: 5, // 移动 5px 后触发拖拽
         },
       }),
     );
