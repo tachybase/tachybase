373fb711dad5489512ea02e01e245adb527fc414
dududuna <wwwjh0710@163.com>
2025-01-16 17:53:15 +0800

    fix: change the summary chart to a tree level structure

--- packages/plugin-block-charts/src/client/chart/antd/GroupedTable.ts
+++ packages/plugin-block-charts/src/client/chart/antd/GroupedTable.ts
@@ -7,12 +7,61 @@ import { AntdChart } from './antd';
 
 export class GroupedTable extends AntdChart {
   constructor() {
-    super({ name: 'groupedTable', title: 'GroupedTable', Component: AntdTable, config: ['seriesField'] });
+    super({
+      name: 'groupedTable',
+      title: 'GroupedTable',
+      Component: AntdTable,
+      config: [
+        {
+          categoryField: {
+            title: '{{t("Category Field")}}',
+            type: 'array',
+            'x-decorator': 'FormItem',
+            'x-component': 'ArrayItems',
+            items: {
+              type: 'void',
+              'x-component': 'Space',
+              properties: {
+                sort: {
+                  type: 'void',
+                  'x-decorator': 'FormItem',
+                  'x-component': 'ArrayItems.SortHandle',
+                },
+                input: {
+                  type: 'string',
+                  'x-decorator': 'FormItem',
+                  'x-component': 'Select',
+                  'x-reactions': '{{ useChartFields }}',
+                  'x-component-props': {
+                    style: {
+                      minWidth: '200px',
+                    },
+                  },
+                  required: true,
+                },
+                remove: {
+                  type: 'void',
+                  'x-decorator': 'FormItem',
+                  'x-component': 'ArrayItems.Remove',
+                },
+              },
+            },
+            properties: {
+              add: {
+                type: 'void',
+                title: '{{t("Add")}}',
+                'x-component': 'ArrayItems.Addition',
+              },
+            },
+          },
+        },
+      ],
+    });
   }
 
   getProps({ data, fieldProps, general, advanced, ctx }: RenderProps) {
     const { transform, config, service } = ctx;
-    const seriesField = config?.general?.seriesField;
+    const categoryField = config?.general?.categoryField;
     const measures = service?.params.find((item) => typeof item === 'object')?.measures;
     const columns = data.length
       ? Object.keys(data[0]).map((item) => ({
@@ -22,8 +71,6 @@ export class GroupedTable extends AntdChart {
           calculate: true,
         }))
       : [];
-    const dataSource = [];
-    let key = 0;
     data.forEach((item: any, index) => {
       Object.keys(item).forEach((key: string) => {
         const props = fieldProps[key];
@@ -38,80 +85,9 @@ export class GroupedTable extends AntdChart {
           item[key] = props.transformer(item[key]);
         }
       });
-      const dataValue = dataSource.filter((value) => value[seriesField] === item[seriesField])[0];
-      if (dataValue) {
-        dataSource[dataValue.key].children.push({
-          key: `key${uid()}${uid()}`,
-          ...item,
-        });
-      } else {
-        dataSource.push({
-          key: key,
-          ...item,
-          children: [
-            {
-              key: `key${uid()}`,
-              ...item,
-            },
-          ],
-        });
-        key++;
-      }
     });
-
-    advanced?.columns?.forEach((dataValue) => {
-      if (dataValue.key === seriesField) {
-        return;
-      }
-      dataSource.forEach((value) => {
-        if (measures?.find((item) => item.field?.join('.') === dataValue.key)) {
-          if (isNaN(Number(value[dataValue.key]))) {
-            value[dataValue.key] = 0;
-          }
-          let number: any = transform.filter((value) => value.field === dataValue.key)[0];
-          if (number) {
-            number = number.specific ? number.specific : 3;
-            switch (number) {
-              case 'TwoDigits':
-                number = 2;
-                break;
-              case 'ThreeDigits':
-                number = 3;
-                break;
-              case 'FourDigits':
-                number = 4;
-                break;
-            }
-          } else {
-            number = 3;
-          }
-          const options: Intl.NumberFormatOptions = {
-            style: 'decimal',
-            minimumFractionDigits: number,
-            maximumFractionDigits: number,
-          };
-
-          const numberFormat = new Intl.NumberFormat('zh-CN', options);
-          const num = String(value[dataValue.key]).includes(',')
-            ? String(value[dataValue.key]).replace(/,/g, '')
-            : value[dataValue.key];
-
-          if (!isNaN(num)) {
-            const sum = value.children.reduce((sum, curr) => {
-              const sub = String(curr[dataValue.key]).includes(',')
-                ? String(curr[dataValue.key]).replace(/,/g, '')
-                : isNaN(Number(curr[dataValue.key]))
-                  ? 0
-                  : curr[dataValue.key];
-              return sum + parseFloat(sub);
-            }, 0);
-            value[dataValue.key] = numberFormat.format(sum);
-          }
-        } else {
-          value[dataValue.key] = '';
-        }
-      });
-    });
+    const dataSource = buildTree(data, categoryField);
+    categoryField.forEach((item) => countDataSource(dataSource, measures, transform));
     return {
       bordered: true,
       size: 'middle',
@@ -128,3 +104,93 @@ export class GroupedTable extends AntdChart {
     };
   }
 }
+
+const buildTree = (data, fields) => {
+  const recursiveBuild = (data, fieldIndex) => {
+    if (fieldIndex >= fields.length) {
+      return data.map((item) => ({ key: uid(), ...item }));
+    }
+
+    const field = fields[fieldIndex];
+    const grouped = groupBy(data, field);
+
+    return Object.keys(grouped).map((key) => {
+      const value = [...grouped[key].value];
+      delete grouped[key].value;
+      for (let groupKey in grouped[key]) {
+        if (groupKey !== field) grouped[key][groupKey] = '';
+      }
+      const item = {
+        ...grouped[key],
+        key: key + uid(),
+        children: recursiveBuild(value, fieldIndex + 1),
+      };
+      return item;
+    });
+  };
+
+  const groupBy = (array, key) => {
+    return array.reduce((acc, item) => {
+      const fieldValue = item[key];
+      if (!acc[fieldValue]) acc[fieldValue] = { ...item, value: [] };
+      acc[fieldValue].value.push(item);
+      return acc;
+    }, {});
+  };
+
+  return recursiveBuild(data, 0);
+};
+
+const countDataSource = (dataSource, measures, transform) => {
+  measures?.forEach((dataValue) => {
+    const countData = (data) => {
+      data.forEach((value) => {
+        if (!value.children?.length) return;
+        const dataKey = dataValue.field.join('.');
+        if (isNaN(Number(value[dataKey]))) {
+          value[dataKey] = 0;
+        }
+        let number: any = transform.filter((value) => value.field === dataKey)[0];
+        if (number) {
+          number = number.specific ? number.specific : 3;
+          switch (number) {
+            case 'TwoDigits':
+              number = 2;
+              break;
+            case 'ThreeDigits':
+              number = 3;
+              break;
+            case 'FourDigits':
+              number = 4;
+              break;
+          }
+        } else {
+          number = 3;
+        }
+        const options: Intl.NumberFormatOptions = {
+          style: 'decimal',
+          minimumFractionDigits: number,
+          maximumFractionDigits: number,
+        };
+
+        const numberFormat = new Intl.NumberFormat('zh-CN', options);
+        const num = String(value[dataKey]).includes(',') ? String(value[dataKey]).replace(/,/g, '') : value[dataKey];
+        if (!isNaN(num)) {
+          const sum = value.children.reduce((sum, curr) => {
+            const sub = String(curr[dataKey]).includes(',')
+              ? String(curr[dataKey]).replace(/,/g, '')
+              : isNaN(Number(curr[dataKey]))
+                ? 0
+                : curr[dataKey];
+            return sum + parseFloat(sub);
+          }, 0);
+          value[dataKey] = numberFormat.format(sum || 0);
+        }
+        if (value.children) {
+          countData(value.children);
+        }
+      });
+    };
+    countData(dataSource);
+  });
+};
