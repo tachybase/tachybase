7e40f8d01fd893aad820c317f27ca78dffbb36fc
Toby <2287769986@qq.com>
2025-01-17 10:34:19 +0800

    fix: not show assistant in signin or signup

--- packages/client/src/built-in/assistant/Assistant.provider.tsx
+++ packages/client/src/built-in/assistant/Assistant.provider.tsx
@@ -1,10 +1,15 @@
 import { ToolOutlined } from '@ant-design/icons';
 import { FloatButton } from 'antd';
+import { useLocation } from 'react-router-dom';
 
 import { ACLRolesCheckProvider } from '../acl';
 import { PinnedPluginList } from '../pinned-list';
 
 export const AssistantProvider = ({ children }) => {
+  const location = useLocation();
+  if (location.pathname === '/signin' || location.pathname === '/signup') {
+    return <>{children}</>;
+  }
   return (
     <>
       {children}
