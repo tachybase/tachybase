f26f06b9c06c8c534741d4c5fd297f7fffe63550
Toby <2287769986@qq.com>
2025-01-17 10:20:43 +0800

    Revert "fix: can not signup"
    This reverts commit 9c3aac6417185dcb3ad57f0868ac6d56e39c24c6.

--- packages/client/src/built-in/assistant/Assistant.provider.tsx
+++ packages/client/src/built-in/assistant/Assistant.provider.tsx
@@ -1,15 +1,18 @@
 import { ToolOutlined } from '@ant-design/icons';
 import { FloatButton } from 'antd';
 
+import { ACLRolesCheckProvider } from '../acl';
 import { PinnedPluginList } from '../pinned-list';
 
 export const AssistantProvider = ({ children }) => {
   return (
     <>
       {children}
-      <FloatButton.Group trigger="click" type="default" style={{ right: 24, zIndex: 1250 }} icon={<ToolOutlined />}>
-        <PinnedPluginList belongToFilter="hoverbutton" />
-      </FloatButton.Group>
+      <ACLRolesCheckProvider>
+        <FloatButton.Group trigger="click" type="default" style={{ right: 24, zIndex: 1250 }} icon={<ToolOutlined />}>
+          <PinnedPluginList belongToFilter="hoverbutton" />
+        </FloatButton.Group>
+      </ACLRolesCheckProvider>
     </>
   );
 };
