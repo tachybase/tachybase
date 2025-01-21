a105fe314543fa2476dba99a6b96fde1c83280dd
bai.zixv <bai.zixv@foxmail.com>
2025-01-21 11:44:47 +0800

    fix: approval, fix form

--- packages/plugin-workflow-approval/src/client/usage/pc/block/todos-table/providers/ApprovalUpdateForm.provider.tsx
+++ packages/plugin-workflow-approval/src/client/usage/pc/block/todos-table/providers/ApprovalUpdateForm.provider.tsx
@@ -3,6 +3,7 @@ import { APPROVAL_ACTION_STATUS } from '../../../constants';
 import { useContextApprovalRecords } from './ApprovalExecutions.provider';
 
 export const ProviderApprovalUpdateForm = (props) => {
+  // TODO: 临时修复, 重构过程中将同类上下文合并
   const { status } = useContextApprovalRecords();
   const { status: statusMobile } = useContextApprovalExecution();
   if (status === APPROVAL_ACTION_STATUS.PENDING || statusMobile === APPROVAL_ACTION_STATUS.PENDING) {
