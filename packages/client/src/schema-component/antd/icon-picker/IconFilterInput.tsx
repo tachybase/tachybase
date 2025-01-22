5ca12ca6b038c65a37e84cdf30445684f6093776
bai.zixv <bai.zixv@foxmail.com>
2025-01-22 14:06:51 +0800

    feat: iconPicker, translate

--- packages/client/src/schema-component/antd/icon-picker/IconFilterInput.tsx
+++ packages/client/src/schema-component/antd/icon-picker/IconFilterInput.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
-
 import { Input } from 'antd';
 import _ from 'lodash';
+import { useTranslation } from 'react-i18next';
 
 export const IconFilterInput = (props) => {
   const { changeFilterKey } = props;
+  const { t } = useTranslation();
   const onChange = _.debounce((e) => {
     const inputValue = e.target.value;
     changeFilterKey(inputValue);
   }, 100);
 
-  return <Input allowClear placeholder={`Antd的key,例如"ApiOutlined",忽略大小写`} onChange={onChange} />;
+  return (
+    <Input allowClear placeholder={t("The key in Antd, such as 'ApiOutlined', ignoring case")} onChange={onChange} />
+  );
 };
