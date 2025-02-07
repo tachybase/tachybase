ceef297fdaf5aff01121eb47d8012990f4f0bfe4
Toby <2287769986@qq.com>
2025-02-07 17:52:47 +0800

    fix: baseUrl not show

--- packages/plugin-data-source-common/src/client/features/rest-api/request-configs/method-path/FieldPath.view.tsx
+++ packages/plugin-data-source-common/src/client/features/rest-api/request-configs/method-path/FieldPath.view.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
-import { SchemaComponent, useSchemaComponentContext } from '@tachybase/client';
+import { SchemaComponent, useApp } from '@tachybase/client';
 
 import { useTranslation } from '../../../../locale';
 import { getSchemaFieldPath } from './FieldPath.schema';
 
 export const ViewFieldPath = (props) => {
   const { t } = useTranslation();
-  const scCtx: any = useSchemaComponentContext();
-  const { path, setFormValue } = props;
+  const { path, setFormValue, dataSourceKey } = props;
+
+  const app = useApp();
+  const baseUrl = app.dataSourceManager.getDataSource(dataSourceKey)?.getOptions()?.options?.baseUrl;
 
   const schema = getSchemaFieldPath({
     t,
     path,
     setFormValue,
-    scCtx,
+    baseUrl,
   });
 
   return <SchemaComponent schema={schema} />;
