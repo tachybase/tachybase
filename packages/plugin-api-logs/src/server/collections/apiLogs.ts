abe3a1f70f5ed8c09875cefd8635e19441857eb9
Toby <2287769986@qq.com>
2025-02-07 19:21:02 +0800

    feat: support api logs instead of audit logs

---
+++ packages/plugin-api-logs/src/server/collections/apiLogs.ts
@@ -0,0 +1,51 @@
+import { CollectionOptions } from '@tachybase/database';
+
+export default {
+  dumpRules: {
+    group: 'log',
+  },
+  name: 'apiLogs',
+  createdBy: false,
+  updatedBy: false,
+  updatedAt: false,
+  shared: true,
+  fields: [
+    {
+      type: 'date',
+      name: 'createdAt',
+    },
+    {
+      type: 'string',
+      name: 'action',
+    },
+    {
+      type: 'string',
+      name: 'recordId',
+      index: true,
+    },
+    {
+      type: 'string',
+      name: 'collectionName',
+    },
+    {
+      type: 'belongsTo',
+      name: 'collection',
+      target: 'collections',
+      targetKey: 'name',
+      sourceKey: 'id',
+      foreignKey: 'collectionName',
+      constraints: false,
+    },
+    {
+      type: 'hasMany',
+      name: 'changes',
+      target: 'apiLogsChanges',
+      foreignKey: 'logId',
+    },
+    {
+      type: 'belongsTo',
+      name: 'user',
+      target: 'users',
+    },
+  ],
+} as CollectionOptions;
