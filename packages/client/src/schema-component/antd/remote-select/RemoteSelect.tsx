be36404118c0ed875b8cf78faec7ae43b0c5c936
dududuna <wwwjh0710@163.com>
2025-02-10 13:29:24 +0800

    fix: replace non paginated query parameters

--- packages/client/src/schema-component/antd/remote-select/RemoteSelect.tsx
+++ packages/client/src/schema-component/antd/remote-select/RemoteSelect.tsx
@@ -134,16 +134,21 @@ const InternalRemoteSelect = connect(
       },
       [targetField?.uiSchema, fieldNames],
     );
+    const params = {
+      ...service?.params,
+      filter: service?.params?.filter,
+    };
+    if (fieldNames['formula']) {
+      params['paginate'] = false;
+    } else {
+      params['pageSize'] = 200;
+    }
     const { data, run, loading } = useRequest(
       {
         action: 'list',
         ...service,
         headers,
-        params: {
-          pageSize: fieldNames['formula'] ? 9999 : 200,
-          ...service?.params,
-          filter: service?.params?.filter,
-        },
+        params,
       },
       {
         manual,
