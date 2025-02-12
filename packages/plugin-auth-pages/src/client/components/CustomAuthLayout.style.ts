d2853cda02c76235c5c3d200b275c396b613a22f
bai.zixv <bai.zixv@foxmail.com>
2025-02-12 15:35:47 +0800

    feat: account logo

--- packages/plugin-auth-pages/src/client/components/CustomAuthLayout.style.ts
+++ packages/plugin-auth-pages/src/client/components/CustomAuthLayout.style.ts
@@ -7,6 +7,18 @@ export const useStyles = createStyles(({ css, token }) => {
       height: 100vh;
       background-color: #fff;
 
+      .account-logo {
+        position: absolute;
+        left: 60px;
+        top: 40px;
+        height: 48px;
+        cursor: pointer;
+      }
+
+      .logo-img {
+        height: 48px;
+      }
+
       .account-bg {
         width: 100%;
         height: 100%;
@@ -14,6 +26,28 @@ export const useStyles = createStyles(({ css, token }) => {
         background-size: cover;
         background-repeat: no-repeat;
         background-image: url('https://tachybase-1321007335.cos.ap-shanghai.myqcloud.com/7b4b4565b3b11041505edfbc0c770646.webp');
+      }
+
+      .account-wrapper {
+        position: relative;
+        display: grid;
+        place-items: center;
+      }
+
+      .account-container {
+        position: relative;
+        margin: 0 auto;
+        width: 400px;
+        max-width: 480px;
+        transform: translateY(-50%);
+      }
+
+      .account-poweredby {
+        position: absolute;
+        bottom: 24px;
+        width: 100%;
+        left: 0;
+        text-align: center;
       }
     `,
   };
--- packages/plugin-auth-pages/src/client/components/CustomAuthLayout.tsx
+++ packages/plugin-auth-pages/src/client/components/CustomAuthLayout.tsx
@@ -1,10 +1,9 @@
-import { css, PoweredBy } from '@tachybase/client';
+import { PoweredBy } from '@tachybase/client';
 import { AuthenticatorsContext } from '@tachybase/module-auth/client';
 
 import { Col, Row } from 'antd';
 import { Outlet } from 'react-router-dom';
 
-import accountBg from '../assets/account-bg.webp';
 import { useProps } from './CustomAuthLayout.props';
 import { useStyles } from './CustomAuthLayout.style';
 
@@ -13,44 +12,25 @@ export const CustomAuthLayout = () => {
   const { styles } = useStyles();
   return (
     <Row className={styles.customAuthLayout}>
-      <Col xs={{ span: 0 }} md={{ span: 12 }}>
+      <Col xs={{ span: 0 }} md={{ span: 14 }}>
+        <a className="account-logo" href="https://www.tachybase.com" target="_self">
+          <img
+            className="logo-img"
+            src="https://tachybase-1321007335.cos.ap-shanghai.myqcloud.com/ff3d4d2bc63ed3b26ec1852fcc173db4.png"
+            alt=""
+          />
+        </a>
         <div className="account-bg"></div>
-        {/* <img
-          src={accountBg}
-          style={{
-            objectFit: 'cover',
-            objectPosition: 'center',
-            width: '100%',
-            height: '100%',
-            maxWidth: '100%',
-            display: 'block',
-            verticalAlign: 'middle',
-          }}
-        /> */}
       </Col>
-      <Col xs={{ span: 24 }} md={{ span: 12 }}>
-        <div
-          style={{
-            maxWidth: 320,
-            margin: '0 auto',
-            paddingTop: '20vh',
-          }}
-        >
+      <Col className="account-wrapper" xs={{ span: 24 }} md={{ span: 10 }}>
+        <div className="account-container">
           <h1>{title}</h1>
           <AuthenticatorsContext.Provider value={authenticators as any}>
             <Outlet />
           </AuthenticatorsContext.Provider>
-          <div
-            className={css`
-              position: absolute;
-              bottom: 24px;
-              width: 100%;
-              left: 0;
-              text-align: center;
-            `}
-          >
-            <PoweredBy />
-          </div>
+        </div>
+        <div className={'account-poweredby'}>
+          <PoweredBy />
         </div>
       </Col>
     </Row>
