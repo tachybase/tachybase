3494d62b740bc8a69aca179cca02cdf2542d50ce
sealday <sealday@gmail.com>
2025-02-16 01:19:35 +0800

    refactor: add core

---
+++ packages/schema/src/core/models/Graph.ts
@@ -0,0 +1,55 @@
+import { batch, define } from '../../reactive';
+import { each, FormPath } from '../../shared';
+import { isArrayFieldState, isFieldState, isFormState, isObjectFieldState } from '../shared/checkers';
+import { IFormGraph } from '../types';
+import { Form } from './Form';
+
+export class Graph {
+  form: Form;
+
+  constructor(form: Form) {
+    this.form = form;
+    define(this, {
+      setGraph: batch,
+    });
+  }
+
+  getGraph = (): IFormGraph => {
+    const graph = {};
+    graph[''] = this.form.getState();
+    each(this.form.fields, (field: any, identifier) => {
+      graph[identifier] = field.getState();
+    });
+    return graph;
+  };
+
+  setGraph = (graph: IFormGraph) => {
+    const form = this.form;
+    const createField = (identifier: string, state: any) => {
+      const address = FormPath.parse(identifier);
+      const name = address.segments[address.segments.length - 1];
+      const basePath = address.parent();
+      if (isFieldState(state)) {
+        return this.form.createField({ name, basePath });
+      } else if (isArrayFieldState(state)) {
+        return this.form.createArrayField({ name, basePath });
+      } else if (isObjectFieldState(state)) {
+        return this.form.createObjectField({ name, basePath });
+      } else {
+        return this.form.createVoidField({ name, basePath });
+      }
+    };
+    each(graph, (state, address) => {
+      if (isFormState(state)) {
+        form.setState(state);
+      } else {
+        const field = form.fields[address];
+        if (field) {
+          field.setState(state);
+        } else {
+          createField(address, state).setState(state);
+        }
+      }
+    });
+  };
+}
