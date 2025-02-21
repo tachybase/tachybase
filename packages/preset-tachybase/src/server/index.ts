3553b5986706244886eb6265c3259a5743a76032
Toby <2287769986@qq.com>
2025-02-21 19:45:45 +0800

    fix: api-logs default false

--- packages/preset-tachybase/src/server/index.ts
+++ packages/preset-tachybase/src/server/index.ts
@@ -49,7 +49,6 @@ export class PresetTachyBase extends Plugin {
     ['log-viewer', '0.22.67', true],
     ['otp', '0.22.67', true],
     ['full-text-search', '0.23.24', true],
-    ['api-logs', '0.23.48', true],
     // default disable
     ['adapter-bullmq', '0.21.76', false],
     ['adapter-red-node', '0.22.8', false],
@@ -84,6 +83,7 @@ export class PresetTachyBase extends Plugin {
     ['ai-chat', '0.23.8', false],
     ['department', '0.23.22', false],
     ['workflow-analysis', '0.23.41', false],
+    ['api-logs', '0.23.49', false],
   ];
 
   get localPlugins() {
