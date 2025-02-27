2c9e30a49f7b8a1f099b6cd6f6544f0f4d26c69f
bai.zixv <bai.zixv@foxmail.com>
2025-02-27 16:40:21 +0800

    feat: remove box-shadow

--- packages/client/src/schema-component/antd/page/style.ts
+++ packages/client/src/schema-component/antd/page/style.ts
@@ -44,7 +44,6 @@ export const getStyles = genStyleHook('tb-page', (token) => {
 
       '.tb-page-header-wrapper': {
         zIndex: 10,
-        boxShadow: '1px 1px 10px 1px rgba(0, 0, 0, 0.1)',
       },
 
       '.pageHeaderCss': {
