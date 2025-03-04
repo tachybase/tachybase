87b48ca7183241332656edf245cebf72b3cd1431
Toby <2287769986@qq.com>
2025-03-04 13:51:01 +0800

    fix: dependencies

--- packages/plugin-multi-process-app/src/server/plugin.ts
+++ packages/plugin-multi-process-app/src/server/plugin.ts
@@ -1,11 +1,25 @@
 import { Plugin } from '@tachybase/server';
 
+import proxy from 'koa-proxies';
+
 export class MultiProcessAppServer extends Plugin {
   async afterAdd() {}
 
   async beforeLoad() {}
 
-  async load() {}
+  async load() {
+    const repo = this.db.getRepository('process_apps');
+    const appList = await repo.find();
+    const middlewares: Record<number, any> = {};
+    for (const appItem of appList) {
+      const proxyFunc = proxy('/', {
+        target: appItem.host,
+        changeOrigin: true,
+      });
+      this.app.use(proxyFunc);
+      middlewares[appItem.id] = proxyFunc;
+    }
+  }
 
   async install() {}
 
