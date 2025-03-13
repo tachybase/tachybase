bea67bb969e3fd9c03b240512477453132bb867f
Toby <2287769986@qq.com>
2025-03-13 09:45:29 +0800

    fix: upper file name

---
+++ packages/plugin-password-policy/src/server/actions/PasswordPolicyController.ts
@@ -0,0 +1,51 @@
+import { Context, Next } from '@tachybase/actions';
+import { Action, Controller, Inject } from '@tachybase/utils';
+
+import { PasswordPolicyService } from '../services/PasswordPolicyService';
+
+@Controller('passwordPolicy')
+export class PasswordPolicyController {
+  @Inject(() => PasswordPolicyService)
+  passwordPolicyService: PasswordPolicyService;
+
+  @Action('get')
+  async getConfig(ctx: Context, next: Next) {
+    const repo = ctx.db.getRepository('passwordPolicy');
+    const data = await repo.findOne();
+    ctx.body = data;
+    return next();
+  }
+
+  @Action('put')
+  async setConfiguration(ctx: Context, next: Next) {
+    const params = ctx.action.params;
+    let transaction;
+    try {
+      const transaction = await ctx.db.sequelize.transaction();
+      const repo = ctx.db.getRepository('passwordPolicy');
+      const existOne = await repo.findOne({
+        transaction,
+      });
+      let data;
+      if (!existOne) {
+        data = await repo.create({
+          values: params.values,
+          transaction,
+        });
+      } else {
+        data = await repo.update({
+          filterByTk: params.values.id,
+          values: params.values,
+          transaction,
+        });
+      }
+      await transaction.commit();
+      await this.passwordPolicyService.refreshConfig(data);
+      ctx.body = data;
+    } catch (err) {
+      ctx.app.logger.error('put password policy config error', err);
+      transaction?.rollback();
+    }
+    return next();
+  }
+}
