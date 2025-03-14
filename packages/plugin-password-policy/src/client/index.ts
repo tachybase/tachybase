3cf0215e7359d286f4c66eaf170d13f3b104be2f
Toby <2287769986@qq.com>
2025-03-14 15:57:40 +0800

    fix: bug

--- packages/plugin-password-policy/src/client/index.ts
+++ packages/plugin-password-policy/src/client/index.ts
@@ -1,23 +1,41 @@
+import React from 'react';
 import { Plugin } from '@tachybase/client';
 
-import { PasswordInput, validatePasswordStrength } from './components/PasswordInput';
-import { PasswordStrengthIndicator } from './components/PasswordStrengthIndicator';
-import { usePasswordValidator } from './hooks/usePasswordValidator';
+import { IPFilterForm } from './IPFilterForm';
+import { tval } from './locale';
+import { PasswordAttemptForm } from './PasswordAttemptForm';
+import PasswordStrengthSettingsPage from './PasswordStrengthSettingsForm';
+import { UserLockTable } from './UserLocksTable';
 
-export { PasswordInput, validatePasswordStrength } from './components/PasswordInput';
-export { PasswordStrengthIndicator } from './components/PasswordStrengthIndicator';
-export { usePasswordValidator } from './hooks/usePasswordValidator';
-
-export class PasswordPolicyClientPlugin extends Plugin {
+export class PasswordPolicyClient extends Plugin {
   async load() {
-    // 注册组件
-    this.app.registerComponents({
-      PasswordInput: PasswordInput,
-      PasswordStrengthIndicator: PasswordStrengthIndicator,
+    this.app.systemSettingsManager.add('security.password-attempt', {
+      icon: 'SettingOutlined',
+      title: tval('Password policy'),
+      Component: PasswordAttemptForm,
+      aclSnippet: `pm.security.password-attempt`,
+    });
+
+    this.app.systemSettingsManager.add('security.user-lock', {
+      icon: 'UserOutlined',
+      title: tval('User lock'),
+      Component: UserLockTable,
+      aclSnippet: `pm.security.user-lock`,
+    });
+
+    this.app.systemSettingsManager.add('security.ip-filter', {
+      icon: 'GlobalOutlined',
+      title: tval('IP filter'),
+      Component: IPFilterForm,
+      aclSnippet: `pm.security.ip-filter`,
+    });
+
+    this.app.systemSettingsManager.add('security.password-strength', {
+      icon: 'LockOutlined',
+      title: tval('Password strength'),
+      Component: PasswordStrengthSettingsPage,
     });
   }
 }
 
-export default async function (options = {}) {
-  return new PasswordPolicyClientPlugin(options);
-}
+export default PasswordPolicyClient;
