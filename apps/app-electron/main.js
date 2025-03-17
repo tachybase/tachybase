54d86bfcb015c4489d1c41fe8e4ad5a7d900322a
bai.zixv <bai.zixv@foxmail.com>
2025-03-17 13:43:56 +0800

    feat: electron app

---
+++ apps/app-electron/main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  win.loadURL('http://localhost:3000'); // 假设你的前端应用在这个端口运行
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
