d0c0cec82890ede424fad55ac11cef2d7bf3a606
dududuna <wwwjh0710@163.com>
2025-03-17 19:05:28 +0800

    fix: the form does not have a default value selection (#324)

--- packages/client/src/modules/blocks/data-blocks/form/fieldSettingsFormItem.tsx
+++ packages/client/src/modules/blocks/data-blocks/form/fieldSettingsFormItem.tsx
@@ -9,14 +9,16 @@ import { SchemaSettings } from '../../../../application/schema-settings/SchemaSe
 import { useFormBlockContext } from '../../../../block-provider';
 import { useCollection_deprecated, useCollectionManager_deprecated } from '../../../../collection-manager';
 import { useFieldComponentName } from '../../../../common/useFieldComponentName';
-import { useContextConfigSetting } from '../../../../data-source';
+import { fieldComponentSettingsItem, useContextConfigSetting } from '../../../../data-source';
 import { useDesignable, useValidateSchema } from '../../../../schema-component';
 import { useIsFormReadPretty } from '../../../../schema-component/antd/form-item/FormItem.Settings';
 import { getTempFieldState } from '../../../../schema-component/antd/form-v2/utils';
-import { isPatternDisabled } from '../../../../schema-settings';
-import { useIsAllowToSetDefaultValue } from '../../../../schema-settings/hooks/useIsAllowToSetDefaultValue';
+import {
+  isPatternDisabled,
+  SchemaSettingsDefaultValue,
+  useIsAllowToSetDefaultValue,
+} from '../../../../schema-settings';
 import { ActionType } from '../../../../schema-settings/LinkageRules/type';
-import { SchemaSettingsDefaultValue } from '../../../../schema-settings/SchemaSettingsDefaultValue';
 
 export const fieldSettingsFormItem = new SchemaSettings({
   name: 'fieldSettings:FormItem',
@@ -481,6 +483,11 @@ export const fieldSettingsFormItem = new SchemaSettings({
               const validateSchema = useValidateSchema();
               return form && !isFormReadPretty && validateSchema;
             },
+          },
+          { ...fieldComponentSettingsItem },
+          {
+            name: 'editDefaultValue',
+            Component: SchemaSettingsDefaultValue,
           },
         ];
       },
