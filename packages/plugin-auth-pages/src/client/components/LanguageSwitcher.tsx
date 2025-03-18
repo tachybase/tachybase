d4c049dd58134c6b66169f30b9b513fafd942012
bai.zixv <bai.zixv@foxmail.com>
2025-03-18 18:54:25 +0800

    feat(auth-login): support new style login page (#308)
    * feat(auth-login): update CustomAuthLayout page
    
    * feat: login background page
    
    * feat: account logo
    
    * feat: add LanguageSwitcher simple version component
    
    * docs: todo file
    
    * feat: nothing
    
    * feat: update LanguageSwitcher
    
    * feat: tachybase, auth-page
    
    * feat: auth-pages language switcher
    
    * feat: locale switcher
    
    * feat: language switcher
    
    * feat: tachybase
    
    * fix: ci on dev (#329)
    
    * feat: login, page
    
    * feat: enable auth-pages
    
    * fix: version
    
    ---------
    
    Co-authored-by: wildworker <31757692+wildworker@users.noreply.github.com>
    Co-authored-by: Toby <2287769986@qq.com>

---
+++ packages/plugin-auth-pages/src/client/components/LanguageSwitcher.tsx
@@ -0,0 +1,35 @@
+import { useMemo } from 'react';
+import { locale, SelectWithTitle, useAPIClient, useSystemSettings, useTranslation } from '@tachybase/client';
+
+import { useStyles } from './LanguageSwitcher.style';
+
+export const LanguageSwitcher = () => {
+  const { styles } = useStyles();
+  const { i18n } = useTranslation();
+  const api = useAPIClient();
+  const { data } = useSystemSettings();
+  const enabledLanguages: string[] = useMemo(() => data?.data?.enabledLanguages || [], [data?.data?.enabledLanguages]);
+
+  const options = useMemo(
+    () =>
+      Object.keys(locale)
+        .filter((lang) => enabledLanguages.includes(lang))
+        .map((lang) => ({
+          label: locale[lang].label,
+          value: lang,
+        })),
+    [locale, enabledLanguages],
+  );
+
+  return (
+    <SelectWithTitle
+      title={<span className={styles.iconGlobe}></span>}
+      defaultValue={i18n.language}
+      options={options}
+      onChange={async (lang) => {
+        api.auth.setLocale(lang);
+        window.location.reload();
+      }}
+    />
+  );
+};
---
+++ packages/preset-tachybase/src/server/migrations/20250318171601-enable-packages.ts
@@ -0,0 +1,26 @@
+import { Migration } from '@tachybase/server';
+
+export default class extends Migration {
+  on = 'afterLoad'; // 'beforeLoad' or 'afterLoad'
+  appVersion = '<0.23.65';
+
+  async up() {
+    const isExisted = await this.pm.repository.findOne({
+      filter: {
+        packageName: '@tachybase/plugin-auth-pages',
+      },
+    });
+
+    if (isExisted) {
+      await this.pm.repository.update({
+        filter: {
+          packageName: '@tachybase/plugin-auth-pages',
+        },
+        values: {
+          enabled: true,
+          installed: true,
+        },
+      });
+    }
+  }
+}
