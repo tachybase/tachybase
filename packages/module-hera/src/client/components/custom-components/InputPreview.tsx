69a0675dc477ab84db1317a82dd6c961716452c5
dududuna <wwwjh0710@163.com>
2025-03-20 16:59:48 +0800

    fix: add component

--- packages/module-hera/src/client/components/custom-components/InputPreview.tsx
+++ packages/module-hera/src/client/components/custom-components/InputPreview.tsx
@@ -2,7 +2,6 @@ import { observer } from '@tachybase/schema';
 
 export const InputPreview = observer(
   (props) => {
-    console.log('🚀 ~ InputPreview ~ props:', props);
     return <>111</>;
   },
   { displayName: 'Input.Preview' },
