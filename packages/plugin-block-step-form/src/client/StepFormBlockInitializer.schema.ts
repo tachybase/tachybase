f70c72b7418f096c4779bc1a2b8e35c5386f3c6e
bai.zixv <bai.zixv@foxmail.com>
2025-03-31 17:05:18 +0800

    feat: step form block initializer

---
+++ packages/plugin-block-step-form/src/client/StepFormBlockInitializer.schema.ts
@@ -0,0 +1,9 @@
+export default {
+  type: 'void',
+  'x-toolbar': 'BlockSchemaToolbar',
+  'x-settings': 'blockSettings:createForm',
+  'x-component': 'CardItem',
+  'x-decorator': 'FormBlockProvider',
+  'x-use-decorator-props': 'useCreateFormBlockDecoratorProps',
+  properties: {},
+};
--- packages/plugin-block-step-form/src/client/StepFormBlockInitializer.tsx
+++ packages/plugin-block-step-form/src/client/StepFormBlockInitializer.tsx
@@ -1,13 +1,26 @@
-import { DataBlockInitializer, Icon, useSchemaInitializerItem } from '@tachybase/client';
+import {
+  DataBlockInitializer,
+  Icon,
+  useCollectionManager,
+  useSchemaInitializer,
+  useSchemaInitializerItem,
+} from '@tachybase/client';
+
+import schema from './StepFormBlockInitializer.schema';
 
 const StepFormBlockInitializer = () => {
-  const commonItemConfig = useSchemaInitializerItem();
+  const { insert } = useSchemaInitializer();
+  const cm = useCollectionManager();
+  const itemConfig = useSchemaInitializerItem();
 
-  const handleCreateBlockSchema = () => {};
+  const handleCreateBlockSchema = async ({ item }) => {
+    const collection = cm.getCollection(item.name);
+    insert(schema);
+  };
 
   return (
     <DataBlockInitializer
-      {...commonItemConfig}
+      {...itemConfig}
       componentType="StepForm"
       icon={<Icon type="RightSquareOutlined" />}
       onCreateBlockSchema={handleCreateBlockSchema}
