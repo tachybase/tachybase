4bf11e9227dd55dae8dddf9757fc4ca5e4cd28fb
bai.zixv <bai.zixv@foxmail.com>
2025-04-09 16:43:16 +0800

    refactor: refactor workflow-approval plugin

--- packages/plugin-workflow-approval/src/client/usage/h5/locale.tsx
+++ packages/plugin-workflow-approval/src/client/usage/h5/locale.tsx
@@ -1,11 +1,1 @@
-import { tval as nTval } from '@tachybase/client';
-
 export const NAMESPACE = '@tachybase/plugin-workflow-approval';
-
-export const tval = (key: string, haveNamespace: boolean = true) => {
-  if (haveNamespace) {
-    return nTval(key, { ns: NAMESPACE });
-  } else {
-    return nTval(key);
-  }
-};
