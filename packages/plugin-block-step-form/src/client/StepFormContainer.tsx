e6c7b1bc8d94e5f3f4969b549398dbaaa99b27f2
bai.zixv <bai.zixv@foxmail.com>
2025-04-15 16:48:29 +0800

    feat: step form

--- packages/plugin-block-step-form/src/client/StepFormContainer.tsx
+++ packages/plugin-block-step-form/src/client/StepFormContainer.tsx
@@ -1,18 +1,60 @@
-import { Steps } from 'antd';
+import { useState } from 'react';
+
+import { Button, message, Steps } from 'antd';
+
+const steps = [
+  {
+    title: 'Step 11',
+    content: 'Content1',
+  },
+  {
+    title: 'Step 2',
+    content: 'Content2',
+  },
+  {
+    title: 'Step 3',
+    content: 'Content3',
+  },
+];
 
 const StepFormContainer = () => {
-  const items = [
-    {
-      title: 'Step 1',
-    },
-    {
-      title: 'Step 2',
-    },
-    {
-      title: 'Step 3',
-    },
-  ];
-  return <Steps items={items} />;
+  const [current, setCurrent] = useState(0);
+
+  const next = () => {
+    setCurrent(current + 1);
+  };
+
+  const prev = () => {
+    setCurrent(current - 1);
+  };
+  const items = steps.map((item) => ({
+    key: item.title,
+    title: item.title,
+  }));
+
+  return (
+    <>
+      <Steps current={current} items={items} />
+      <div>{steps[current].content}</div>
+      <div className="steps-action">
+        {current < steps.length - 1 && (
+          <Button type="primary" onClick={() => next()}>
+            Next
+          </Button>
+        )}
+        {current === steps.length - 1 && (
+          <Button type="primary" onClick={() => message.success('Processing complete!')}>
+            Done
+          </Button>
+        )}
+        {current > 0 && (
+          <Button style={{ margin: '0 8px' }} onClick={() => prev()}>
+            Previous
+          </Button>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default StepFormContainer;
