e417727919d00e0cd2a2f9f86071b43520af33f9
bai.zixv <bai.zixv@foxmail.com>
2025-04-23 15:00:36 +0800

    feat: step form

--- packages/plugin-block-step-form/src/client/components/StepsStepTitleToolbar.tsx
+++
@@ -1,8 +0,0 @@
-import { SchemaToolbar } from '@tachybase/client';
-
-import { stepTitleSettings } from '../settings/stepTitle';
-
-// TODO: 移动位置, 另外归类
-export const StepsStepTitleToolbar = () => {
-  return <SchemaToolbar draggable={true} initializer={false} settings={stepTitleSettings.name} />;
-};
