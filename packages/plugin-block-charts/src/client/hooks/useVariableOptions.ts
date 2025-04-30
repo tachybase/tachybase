c42d26515325aba7fcac92efde1e8571428d5f60
Toby <2287769986@qq.com>
2025-04-30 17:43:59 +0800

    fix:  protocol bug

--- packages/plugin-block-charts/src/client/hooks/useVariableOptions.ts
+++ packages/plugin-block-charts/src/client/hooks/useVariableOptions.ts
@@ -1,10 +1,10 @@
 import { useMemo } from 'react';
-import { useCurrentUserVariable, useDatetimeVariable } from '@tachybase/client';
+import { useCurrentFormVariable, useCurrentUserVariable, useDatetimeVariable } from '@tachybase/client';
 import { useField } from '@tachybase/schema';
 
 import { useFilterVariable } from './filter';
 
-export const useVariableOptions = () => {
+export const useVariableOptions = (props: any) => {
   const field = useField<any>();
   const { operator, schema } = field.data || {};
   const { currentUserSettings } = useCurrentUserVariable({
@@ -12,11 +12,16 @@ export const useVariableOptions = () => {
     uiSchema: schema,
   });
   const { datetimeSettings } = useDatetimeVariable({ operator, schema });
+
+  const { currentFormSettings } = useCurrentFormVariable({
+    collectionName: props.rootCollection,
+    collectionField: props.collectionField,
+  });
   const filterVariable = useFilterVariable();
 
   const result = useMemo(
-    () => [currentUserSettings, datetimeSettings, filterVariable].filter(Boolean),
-    [datetimeSettings, currentUserSettings, filterVariable],
+    () => [currentUserSettings, datetimeSettings, filterVariable, currentFormSettings].filter(Boolean),
+    [datetimeSettings, currentUserSettings, filterVariable, currentFormSettings],
   );
 
   if (!operator || !schema) return [];
