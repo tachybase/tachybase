13fb19f0f6843cdcb88a1e0e3467a58adc1119e3
wildworker <31757692+wildworker@users.noreply.github.com>
2025-05-14 13:36:20 +0800

    feat: auto backup (#420)
    * feat: auto backup
    
    * fix: bugs
    
    * fix: worker backup
    
    * fix: save
    
    * feat: clean files
    
    * fix: auto backup error and locale

---
+++ packages/module-backup/src/server/model/AutoBackupModel.ts
@@ -0,0 +1,19 @@
+import { Model } from '@tachybase/database';
+
+export class AutoBackupModel extends Model {
+  declare id: number;
+  declare title: string;
+  declare startsOn: Date;
+  declare endsOn?: Date;
+  declare repeat: string;
+  declare enabled: boolean;
+  declare createdAt: Date;
+  declare updatedAt: Date;
+
+  declare allExecuted: number;
+
+  declare password: string;
+  declare dumpRules: string;
+  // 本地最多备份文件数量
+  declare maxNumber: number;
+}
