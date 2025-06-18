5934ed4c29ce66723fe8cb3c9caa0178d383168e
bai.zixv <bai.zixv@foxmail.com>
2025-06-18 14:46:53 +0800

    fix: approval create

--- packages/plugin-workflow-approval/src/client/user-interface/pc/block/table-initiations/apply-button/hooks/useSubmitCreate.tsx
+++ packages/plugin-workflow-approval/src/client/user-interface/pc/block/table-initiations/apply-button/hooks/useSubmitCreate.tsx
@@ -20,7 +20,8 @@ export function useSubmitCreate() {
   const collection = useCollection_deprecated();
   const status = useContextApprovalStatus();
   const apiClient = useAPIClient();
-  const { workflow } = useFlowContext();
+  const flowContext = useFlowContext();
+  const { workflow } = flowContext || {};
 
   return {
     async run(args) {
@@ -35,8 +36,8 @@ export function useSubmitCreate() {
             collectionName: joinCollectionName(collection.dataSource, collection.name),
             data: form.values,
             status: typeof args?.approvalStatus !== 'undefined' ? args?.approvalStatus : status,
-            workflowId: workflow.id,
-            workflowKey: workflow.key,
+            workflowId: workflow?.id,
+            workflowKey: workflow?.key,
           },
         });
         form.reset();
