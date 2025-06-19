efbb1a47c964620fa16c5865d721965ead4e0b71
sealday <sealday@gmail.com>
2025-06-19 23:11:35 +0800

    feat: add plugin devkit

---
+++ packages/plugin-devkit/src/server/plugin.ts
@@ -0,0 +1,19 @@
+import { Plugin } from '@tachybase/server';
+
+export class PluginDevkitServer extends Plugin {
+  async afterAdd() {}
+
+  async beforeLoad() {}
+
+  async load() {}
+
+  async install() {}
+
+  async afterEnable() {}
+
+  async afterDisable() {}
+
+  async remove() {}
+}
+
+export default PluginDevkitServer;
