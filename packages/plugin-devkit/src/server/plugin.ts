68b239ba0c5897183715e6713f3b6974c94d865c
sealday <sealday@gmail.com>
2025-06-20 06:01:39 +0800

    feat: support rsbuild

--- packages/plugin-devkit/src/server/plugin.ts
+++ packages/plugin-devkit/src/server/plugin.ts
@@ -1,11 +1,33 @@
+import path from 'node:path';
 import { Plugin } from '@tachybase/server';
 
+import { createRsbuild, type StartServerResult } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
+
 export class PluginDevkitServer extends Plugin {
+  #result: StartServerResult;
   async afterAdd() {}
 
   async beforeLoad() {}
 
-  async load() {}
+  async load() {
+    const rsbuild = await createRsbuild({
+      rsbuildConfig: {
+        // Rsbuild configuration
+        plugins: [pluginReact()],
+        source: {
+          entry: { index: path.resolve('./web/index.tsx') },
+        },
+      },
+    });
+    this.app.once('afterStart', async () => {
+      this.#result = await rsbuild.startDevServer();
+    });
+
+    this.app.once('beforeStop', async () => {
+      await this.#result?.server.close();
+    });
+  }
 
   async install() {}
 
