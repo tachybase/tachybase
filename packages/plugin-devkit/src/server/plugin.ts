d7febe040596d8a8f4b172f43cc8a8fb0edad0dc
sealday <sealday@gmail.com>
2025-06-21 19:28:29 +0800

    feat: devkit support rslib

--- packages/plugin-devkit/src/server/plugin.ts
+++ packages/plugin-devkit/src/server/plugin.ts
@@ -1,11 +1,27 @@
+import path from 'node:path';
 import { Plugin } from '@tachybase/server';
 
-import { createRsbuild, type StartServerResult } from '@rsbuild/core';
+import { createRsbuild, type RsbuildInstance, type StartServerResult } from '@rsbuild/core';
+import { build, defineConfig } from '@rslib/core';
+import fg from 'fast-glob';
 
 import config from './config';
 
+export const globExcludeFiles = [
+  '!src/**/__tests__',
+  '!src/**/__test__',
+  '!src/**/__e2e__',
+  '!src/**/demos',
+  '!src/**/fixtures',
+  '!src/**/*.mdx',
+  '!src/**/*.md',
+  '!src/**/*.+(test|e2e|spec).+(js|jsx|ts|tsx)',
+];
+const serverGlobalFiles: string[] = ['src/**', '!src/client/**', ...globExcludeFiles];
+
 export class PluginDevkitServer extends Plugin {
   #result: StartServerResult;
+  #pluginBuilding: Awaited<ReturnType<typeof build>>;
   async afterAdd() {}
 
   async beforeLoad() {}
@@ -14,8 +30,30 @@ export class PluginDevkitServer extends Plugin {
     const rsbuild = await createRsbuild({
       rsbuildConfig: config,
     });
+    const cwd = '/Users/seal/Documents/projects/tachybase/packages/plugin-example-hello';
+    const serverFiles = fg.globSync(serverGlobalFiles, { cwd, absolute: true });
+    const pluginConfig = defineConfig({
+      source: {
+        entry: {
+          index: serverFiles,
+        },
+      },
+      lib: [
+        {
+          output: {
+            distPath: {
+              root: path.join(cwd, 'lib'),
+            },
+          },
+          bundle: false,
+          dts: false,
+          format: 'cjs',
+        },
+      ],
+    });
     this.app.once('afterStart', async () => {
       this.#result = await rsbuild.startDevServer();
+      this.#pluginBuilding = await build(pluginConfig);
     });
 
     this.app.once('beforeStop', async () => {
