f4e3e11e75b16542a0fc880082aba66d1330f5cf
sealday <sealday@gmail.com>
2025-06-22 21:25:54 +0800

    chore: remove node_modules

--- apps/engine/src/preload.ts
+++ apps/engine/src/preload.ts
@@ -9,8 +9,8 @@ Error.stackTraceLimit = process.env.ERROR_STACK_TRACE_LIMIT ? +process.env.ERROR
 // 处理 NODE_MODULES_PATH
 // 如果不存在的话，按照约定路径猜测
 if (!process.env.NODE_MODULES_PATH) {
-  if (fs.existsSync(resolve('plugins', 'node_modules'))) {
-    process.env.NODE_MODULES_PATH = resolve('plugins', 'node_modules');
+  if (fs.existsSync(resolve('plugins'))) {
+    process.env.NODE_MODULES_PATH = resolve('plugins');
   } else {
     process.env.NODE_MODULES_PATH = resolve('node_modules');
   }
