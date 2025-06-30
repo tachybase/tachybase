c6674fd6bdcc1c35f882d8b2603181a52fc21a65
WinC159 <1637375142@qq.com>
2025-06-30 17:42:10 +0800

    feat: create file

---
+++ packages/client/src/modules/blocks/data-blocks/form-editor/EditorContent.tsx
@@ -0,0 +1,23 @@
+import { Schema } from '@tachybase/schema';
+
+import { Layout } from 'antd';
+import _, { cloneDeep } from 'lodash';
+
+import { observer } from '../../../../../../schema/src/react';
+import { PageRefreshProvider, usePageRefresh } from '../../../../built-in/dynamic-page/PageRefreshContext';
+import { SchemaComponent } from '../../../../schema-component';
+import { EditableGrid } from './EditableGrid';
+
+interface EditorContentProps {
+  schema: Schema;
+}
+
+export const EditorContent = observer<EditorContentProps>(({ schema }) => {
+  const { Content } = Layout;
+  const { refreshKey } = usePageRefresh();
+  return (
+    <Content key={refreshKey} style={{ padding: '5px', overflow: 'auto' }}>
+      <SchemaComponent schema={schema} components={{ EditableGrid }} />
+    </Content>
+  );
+});
