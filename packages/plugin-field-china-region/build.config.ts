b642ef6700e56b2868a6e94fb6bfe2f6641ef2ed
张琳 Lin Zhang <sealday@gmail.com>
2025-06-30 13:52:02 +0800

    chore: rename @tachybase/app-web to @tego/web (#585)
    * chore: rename @tachybase/app-web to @tego/web
    
    * fix: devkit dep
    
    * fix: plugin build duplicated judgement

--- packages/plugin-field-china-region/build.config.ts
+++ packages/plugin-field-china-region/build.config.ts
@@ -1,7 +1,8 @@
 import { existsSync } from 'node:fs';
 import fs from 'node:fs/promises';
 import path from 'node:path';
-import { defineConfig } from '@tachybase/devkit';
+
+import { defineConfig } from '@tego/devkit';
 
 export default defineConfig({
   afterBuild: async (log) => {
