9607d850c577d850ec5aa5132a9943cff84aab8a
Toby <2287769986@qq.com>
2025-07-02 15:31:24 +0800

    fix: docker copy

--- docker/tego-nightly/docker-entrypoint.sh
+++ docker/tego-nightly/docker-entrypoint.sh
@@ -2,10 +2,10 @@
 set -e
 
 # cp dist files
-# cp -r /app/plugins/builtin/@tego/web/dist/* /app/public/
+cp -r /app/apps/web/dist/* /app/public/
 
 # handle sentry
-original_file="/app/plugins/builtin/@tego/web/dist/index.html"
+original_file="/app/apps/web/dist/index.html"
 sentry_part="/app/public/index.sentry.html"
 output_file="/app/public/index.html"
 
